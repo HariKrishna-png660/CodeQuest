bc5b35d761ee5ead1783b5ffc8564893bda123b6
Harikrishna-coder310 <htalagapu@gmail.com>
2025-08-17 14:03:34 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (25.35%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,23 +1,27 @@
 class Solution {
-    // memoization solution ...
-    public int uniquePaths(int sr,int sc,int dr,int dc,int memo[][]) {
-        if(sr==dr && sc==dc) {
-            return 1;
-        }
-        if(memo[sr][sc]!=0) {
-            return memo[sr][sc];
-        }
+    // tabulation solution ...
+    public int uniquePaths(int dr,int dc,int memo[][]) {
+        for(int i=dr;i>=0;i--) {
+            for(int j=dc;j>=0;j--) {
+                 if(i==dr && j==dc) {
+                   memo[i][j]=1;
+                   continue;
+                 }
+       
         int totalPaths=0;
-        if(sr+1<=dr) {
-            totalPaths += uniquePaths(sr+1,sc,dr,dc,memo);
+        if(i+1<=dr) {
+            totalPaths += memo[i+1][j];//uniquePaths(i+1,sc,dr,dc,memo);
         }
-        if(sc+1<=dc) {
-            totalPaths += uniquePaths(sr,sc+1,dr,dc,memo);
+        if(j+1<=dc) {
+            totalPaths += memo[i][j+1];//uniquePaths(i,sc+1,dr,dc,memo);
         }
-        return memo[sr][sc]=totalPaths;
+        memo[i][j]=totalPaths; 
+            }
+        }
+        return memo[0][0];
     }
     public int uniquePaths(int m, int n) {
         int memo[][]=new int[m][n];
-        return uniquePaths(0,0,m-1,n-1,memo);
+        return uniquePaths(m-1,n-1,memo);
     }
 }
