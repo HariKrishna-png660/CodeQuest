b79b341f9a6217847092b2dbe559204211348a8d
Harikrishna-coder310 <htalagapu@gmail.com>
2025-08-17 18:17:51 +0530

    Time: 6 ms (11.28%) | Memory: 47.3 MB (93.88%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -0,0 +1,29 @@
+class Solution {
+    // tabulation solution ....
+    public int minPathSum(int dr,int dc,int grid[][],int tab[][]) {
+        for(int i=dr;i>=0;i--) {
+            for(int j=dc;j>=0;j--) {
+                if(i==dr && j==dc) {
+                    tab[dr][dc]=grid[dr][dc];
+                    continue;
+                }
+                int minSum=Integer.MAX_VALUE;
+                if(i+1<=dr) {
+                    minSum=Math.min(minSum,tab[i+1][j]);
+                }
+                if(j+1<=dc) {
+                   minSum=Math.min(minSum,tab[i][j+1]);
+                }
+                minSum += grid[i][j];
+                tab[i][j]=minSum;
+            }
+        }
+        return tab[0][0];
+    }
+    public int minPathSum(int[][] grid) {
+        int m=grid.length;
+        int n=grid[0].length;
+        int tab[][]=new int[m][n];
+        return minPathSum(m-1,n-1,grid,tab);
+    }
+}
