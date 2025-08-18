d0b97d71f88cba17765bcb3b59ce3b1f0976cd4c
Harikrishna-coder310 <htalagapu@gmail.com>
2025-08-18 16:39:15 +0530

    Time: 6 ms (11.34%) | Memory: 47.8 MB (43.38%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,30 +1,29 @@
 class Solution {
-    // memoization solution ......
-    public int minPathSum(int sr,int sc,int dr,int dc,int memo[][],int grid[][]) {
-        if(sr==dr && sc==dc) {
-            return grid[dr][dc];
+    // tabulation solution ....
+    public int minPathSum(int dr,int dc,int grid[][],int tab[][]) {
+         for(int j=dc;j>=0;j--) {
+        for(int i=dr;i>=0;i--) {
+                if(i==dr && j==dc) {
+                    tab[dr][dc]=grid[dr][dc];
+                    continue;
+                }
+                int minSum=Integer.MAX_VALUE;
+                if(i+1<=dr) {
+                    minSum=Math.min(minSum,tab[i+1][j]);
+                }
+                if(j+1<=dc) {
+                   minSum=Math.min(minSum,tab[i][j+1]);
+                }
+                minSum += grid[i][j];
+                tab[i][j]=minSum;
+            }
         }
-        if(memo[sr][sc]!=-1) {
-            return memo[sr][sc];
-        }
-        int minSum=Integer.MAX_VALUE;
-        if(sr+1<=dr) {
-            minSum=Math.min(minSum,minPathSum(sr+1,sc,dr,dc,memo,grid));
-        }
-        if(sc+1<=dc) {
-            minSum=Math.min(minSum,minPathSum(sr,sc+1,dr,dc,memo,grid));
-        }
-        return memo[sr][sc]=minSum+grid[sr][sc];
+        return tab[0][0];
     }
     public int minPathSum(int[][] grid) {
         int m=grid.length;
         int n=grid[0].length;
-        int memo[][]=new int[m][n];
-        for(int i=0;i<m;i++) {
-            for(int j=0;j<n;j++) {
-                memo[i][j]=-1;
-            }
-        }
-        return minPathSum(0,0,m-1,n-1,memo,grid);
+        int tab[][]=new int[m][n];
+        return minPathSum(m-1,n-1,grid,tab);
     }
 }
