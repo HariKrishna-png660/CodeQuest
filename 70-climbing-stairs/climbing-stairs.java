00abf00c9c1e9e1d6adca06ad2a6129a6ecf5270
Harikrishna-coder310 <htalagapu@gmail.com>
2025-08-21 18:33:53 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (65.96%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -36,20 +36,38 @@ class Solution {
     // tabulation solution .....
     // time complexity is O(n)
     // space complexity is O(n)
-     public int climbStairs(int n,int tab[]) {
-        for(int i=1;i<=n;i++) {
-            if(i<=2) {
-                tab[i]=i;
-                continue;
-            }
-            int oneStep=tab[i-1];
-            int twoStep=tab[i-2];
-            tab[i]=oneStep+twoStep;
+    //  public int climbStairs(int n,int tab[]) {
+    //     for(int i=1;i<=n;i++) {
+    //         if(i<=2) {
+    //             tab[i]=i;
+    //             continue;
+    //         }
+    //         int oneStep=tab[i-1];
+    //         int twoStep=tab[i-2];
+    //         tab[i]=oneStep+twoStep;
+    //     }
+    //     return tab[n];
+    //  }
+    //  int tab[]=new int[n+1];
+    //   return climbStairs(n,tab);
+    // public int climbStairs(int n) {
+    //    int tab[]=new int[n+1];
+    //    return climbStairs(n,tab);
+    // } 
+    // =======================================
+    // time complexity is O(n)
+    // space complexity is O(1)
+    public int climbStairs(int n) {
+        if(n<=2) {
+            return n;
         }
-        return tab[n];
-     }
-     public int climbStairs(int n) {
-         int tab[]=new int[n+1];
-         return climbStairs(n,tab);
+        int prev2=1;
+        int prev1=2;
+        for(int i=3;i<=n;i++) {
+            int ans=prev2+prev1;
+            prev2=prev1;
+            prev1=ans;
+        }
+        return prev1;
     } 
 }
