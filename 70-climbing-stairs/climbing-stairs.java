49aa52ed275c2316633ab0943b5554c673d7e663
Harikrishna-coder310 <htalagapu@gmail.com>
2025-08-21 18:18:19 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (82.30%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,19 +1,35 @@
 class Solution {
-    // tabulation solution ....
-    public int climbStairs(int n,int tab[]) {
-        for(int i=1;i<=n;i++) {
-            if(i<=2) {
-                tab[i]=i;
-                continue;
-            }
-            int oneStep=tab[i-1];
-            int twoSteps=tab[i-2];
-            tab[i]=oneStep+twoSteps;
+     // recursive solution .....
+    // time complexity is O(2^n)
+    // space complexity is O(n)
+    // public int climbStairs(int n) {
+    //     if(n<=2) {
+    //         return n;
+    //     }
+    //     int oneStepTaken=climbStairs(n-1);
+    //     int twoStepsTaken=climbStairs(n-2);
+    //     int totalSteps=oneStepTaken+twoStepsTaken;
+    //     return totalSteps;
+    // }
+    // ==============================================
+    // memoization solution .......
+    // time complexity is O(n)
+    // space complexity is recursive space O(n) + memo array is O(n) = O(2n) = O(n)
+    public int climbStairs(int n,int memo[]) {
+        if(n<=2) {
+            return memo[n]=n;
         }
-        return tab[n];
+        if(memo[n]!=-1) {
+            return memo[n];
+        }
+        int oneStepTaken=climbStairs(n-1,memo);
+        int twoStepTaken=climbStairs(n-2,memo);
+        int totalStepsTaken=oneStepTaken+twoStepTaken;
+        return memo[n]=totalStepsTaken;
     }
     public int climbStairs(int n) {
-        int tab[]=new int[n+1];
-        return climbStairs(n,tab);
+        int memo[]=new int[n+1];
+        Arrays.fill(memo,-1);
+        return climbStairs(n,memo);
     }
 }
