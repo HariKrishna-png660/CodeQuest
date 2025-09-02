6e353d6759a8c90b7b6f1dab4a30613f6eb968be
Harikrishna-coder310 <htalagapu@gmail.com>
2025-09-02 18:04:43 +0530

    Time: 94 ms (23.15%) | Memory: 44.8 MB (31.10%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,38 +1,24 @@
 class Solution {
-    // memoization solution .......
-    // public int lenghtOfLIS_memo(int memo[],int idx,int nums[]) {
-    //     if(memo[idx]!=0) {
-    //         return memo[idx];
-    //     }
-    //     int ans=1;
-    //     for(int i=idx-1;i>=0;i--) {
-    //         if(nums[i]<nums[idx]) {
-    //           ans=Math.max(ans,lenghtOfLIS_memo(memo,i,nums)+1);
-    //         }
-    //     }
-    //     return memo[idx]=ans;
-    // }
-    public int lengthOfLIS(int[] nums) {
-        // int n=nums.length;
-        // int longestIncreasingSubsequence=0;
-        // int memo[]=new int[n];
-        // for(int i=n-1;i>=0;i--) {
-        //     longestIncreasingSubsequence=Math.max(longestIncreasingSubsequence,lenghtOfLIS_memo(memo,i,nums));
-        // }
-        // return longestIncreasingSubsequence;
-        // tabulation solution .......
-        int n=nums.length;
-        int tab[]=new int[n];
-        Arrays.fill(tab,1);
-        int maxLIS=0;
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<i;j++) {
-                if(nums[j]<nums[i]) {
-                    tab[i]=Math.max(tab[i],tab[j]+1);
-                }
-            }
-            maxLIS=Math.max(maxLIS,tab[i]);
+    public int lengthOfLIS_rec(int nums[],int idx,int memo[]) {
+        if(memo[idx]!=1) {
+            return memo[idx];
         }
-        return maxLIS;
+        int ans=1;
+        for(int j=idx-1;j>=0;j--) {
+            if(nums[j]<nums[idx]) {
+                ans=Math.max(ans,lengthOfLIS_rec(nums,j,memo)+1);
+            }
+        }
+        return memo[idx]=ans;
+    }
+    public int lengthOfLIS(int[] nums) {
+        int n=nums.length;
+        int memo[]=new int[n];
+        Arrays.fill(memo,1);
+        int ans=0;
+        for(int i=n-1;i>=0;i--) {
+            ans=Math.max(ans,lengthOfLIS_rec(nums,i,memo));
+        }
+        return ans;
     }
 }
