ee870e9bb3891bb2dbaf10777f998c881ec2ec6d
HariKrishna-png660 <htalagapu@gmail.com>
2025-09-05 08:19:25 +0530

    Time: 1 ms (38.46%) | Memory: 40.3 MB (98.46%) - LeetSync

---
+++ 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int makeTheIntegerZero(int num1, int num2) {
+    for (long k = 0; k <= 60; ++k) {
+        long target = num1 - k * num2;
+        if (target >= k && Long.bitCount(target) <= k) {
+            return (int) k;
+        }
+    }
+    return -1;
+}
+
+}
