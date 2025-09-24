cc66279398c62ee108c5210de0a0307d09fe13df
HariKrishna-png660 <htalagapu@gmail.com>
2025-09-24 12:47:37 +0530

    Time: 374 ms (40.28%) | Memory: 68.2 MB (30.94%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -1,14 +1,4 @@
 class Solution {
-    class Edge {
-        int u;
-        int v;
-        int w;
-        public Edge(int u,int v,int w) {
-            this.u=u;
-            this.v=v;
-            this.w=w;
-        }
-    }
     static int par[];
     static int size[];
     public int findPar(int u) {
@@ -19,47 +9,49 @@ class Solution {
     }
     public void merge(int p1,int p2) {
         if(size[p1]<size[p2]) {
-           par[p1]=p2;
-           size[p2]+=size[p1];
+            par[p1]=p2;
+            size[p2]+=size[p1];
         }
         else {
-           par[p2]=p1;
-           size[p1] +=size[p2];
+            par[p2]=p1;
+            size[p1]+=size[p2];
         }
     }
     public int minCostConnectPoints(int[][] points) {
         int N=points.length;
-        par=new int[N];
-        size=new int[N];
-        // ArrayList<Edge> graph[]=new ArrayList[N];
-        for(int i=0;i<N;i++) {
-            // graph[i]=new ArrayList<>();
-            par[i]=i;
-            size[i]=1;
-        }
-        // create edges 
-        ArrayList<Edge> edges=new ArrayList<>();
-
+        int edges[][]=new int[N*(N-1)/2][3];
+        int k=0;
         for(int i=0;i<N;i++) {
             for(int j=i+1;j<N;j++) {
                 int u=i;
-                int v=j;
-                int w=Math.abs(points[u][1]-points[v][1])+Math.abs(points[u][0]-points[v][0]);
-                edges.add(new Edge(u,v,w));
+                int v=j; 
+                int w=Math.abs(points[i][0]-points[j][0])+Math.abs( points[i][1]-points[j][1]);
+                edges[k][0]=u;
+                edges[k][1]=v;
+                edges[k][2]=w;
+                k++;
             }
         }
-        edges.sort((e1,e2)->Integer.compare(e1.w,e2.w));
+        Arrays.sort(edges,(a,b)->{
+            return Integer.compare(a[2],b[2]);
+        });
+        par=new int[N];
+        size=new int[N];
+        for(int i=0;i<N;i++) {
+            par[i]=i;
+            size[i]=1;
+        }
         int cost=0;
-        for(Edge edge:edges) {
-            int u=edge.u;
-            int v=edge.v;
-            int w=edge.w;
-            int p1=findPar(u);
-            int p2=findPar(v);
-            if(p1!=p2) {
-               merge(p1,p2);
-               cost+=w;
-            }
+        for(int edge[]:edges) {
+             int u=edge[0];
+             int v=edge[1];
+             int w=edge[2];
+             int p1=findPar(u);
+             int p2=findPar(v);
+             if(p1!=p2) {
+                 merge(p1,p2);
+                 cost+=w;
+             }
         }
         return cost;
     }
