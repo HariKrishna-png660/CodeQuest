d2f5552780c0a883548a0122cbccd61b391eae30
HariKrishna-png660 <htalagapu@gmail.com>
2025-09-29 18:57:08 +0530

    Time: 12 ms (62.21%) | Memory: 45.1 MB (77.39%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,4 +1,5 @@
 class Solution {
+    // this is simply minimize the maximum .....
     public boolean isPossible(int arr[],int min,int h) {
         long count=0;
         for(int i=0;i<arr.length;i++) {
@@ -13,17 +14,17 @@ class Solution {
         return false;
     }
     public int minEatingSpeed(int[] piles, int h) {
-        int si=1;
-        int ei=(int)(1e9);
-        int ans=0;
+        int si=1;   // this is the minimum possible speed with which koko can eat 
+        int ei=(int)(1e9);  // this is the maximum possible speed with which koko can eat
+        int ans=0;     
         while(si<=ei) {
             int mid=(si+ei)/2;
             if(isPossible(piles,mid,h)) {
                 ans=mid;
-                ei=mid-1;
+                ei=mid-1;   // try to get the better minimum 
             }
             else {
-                si=mid+1;
+                si=mid+1;    //  try to search in the right side ....
             }
         }
         return ans;
