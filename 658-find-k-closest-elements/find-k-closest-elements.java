bed26b62a4fce25135ccd52e7cf3423a3735c029
HariKrishna-png660 <htalagapu@gmail.com>
2025-09-29 17:33:05 +0530

    Time: 5 ms (56.70%) | Memory: 46.8 MB (6.69%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,36 +1,36 @@
 class Solution {
-    public int binarySearch(int arr[],int tar) {
-         int si=0;
-         int ei=arr.length-1;
-         while(si<ei) {
+    public int binarySearch(int arr[],int target) {
+        int n=arr.length;
+        int si=0;
+        int ei=n-1;
+        while(si<ei) {
             int mid=(si+ei)/2;
-            if(arr[mid]<tar) {
+            if(arr[mid]<target) {
                 si=mid+1;
             }
             else {
                 ei=mid;
             }
-         }
-         return si;
+        }
+        return ei;
     }
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         int n=arr.length;
-        List<Integer> ans=new ArrayList<>();
+        List<Integer> list=new ArrayList<>();
         for(int val:arr) {
-            ans.add(val);
+            list.add(val);
         }
         if(x<arr[0]) {
-            return ans.subList(0,k);
+            return list.subList(0,k);
         }
-        else if(x>arr[n-1]) {
-            return ans.subList(n-k,n);
+        if(x>arr[n-1]) {
+            return list.subList(n-k,n);
         }
-        
-        int idx=binarySearch(arr,x);
-        
-        int si=Math.max(0,idx-k);
-        int ei=Math.min(idx+k,n-1);
 
+        int idx=binarySearch(arr,x);
+
+        int si=Math.max(0,idx-k);
+        int ei=Math.min(n-1,idx+k);
         while((ei-si+1)>k) {
             if((x-arr[si])<=(arr[ei]-x)) {
                 ei--;
@@ -39,6 +39,6 @@ class Solution {
                 si++;
             }
         }
-        return ans.subList(si,ei+1);
+        return list.subList(si,ei+1);
     }
 }
