206cee9b8fcaa17bb02275edf96e2cb1363c43e0
HariKrishna-png660 <htalagapu@gmail.com>
2025-09-30 17:39:00 +0530

    Time: 2 ms (69.49%) | Memory: 42.2 MB (7.16%) - LeetSync

---
+++ 274-h-index/h-index.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int n=citations.length;
+        int left=0;
+        int right=n-1;
+        while(left<=right) {
+            int mid=(left+right)/2;
+            if(citations[mid]==(n-mid)) {
+                return citations[mid];
+            } 
+            else if(citations[mid]>(n-mid)) {
+                right=mid-1;
+            }
+            else {
+                left=mid+1;
+            }
+        }
+        return n-left;
+    }
+}
