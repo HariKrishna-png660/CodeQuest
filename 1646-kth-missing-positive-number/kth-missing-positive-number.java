1ae04ab6d74802b8c228ae108c164b61748d19fa
HariKrishna-png660 <htalagapu@gmail.com>
2025-09-30 10:47:10 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (19.78%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.java
@@ -1,5 +1,12 @@
 class Solution {
-    public int findKthPositive(int[] arr, int k) {
+    // example
+    //  0 1 2 3 4
+    // given array:[2,3,4,7,11]
+    // original   :[1,2,3,4,5]
+    // diff       :[1,1,1,3,6]
+    // should check the maximum value of diff less than k.
+    // ans is that index+1+k
+        public int findKthPositive(int[] arr, int k) {
         int ans=-1;
         for(int i=0;i<arr.length;i++) {
             int expectedNum=i+1;
