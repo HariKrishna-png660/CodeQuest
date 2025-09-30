8dfaf5feb671f798e758db3f5ea4431f200c95a1
HariKrishna-png660 <htalagapu@gmail.com>
2025-09-30 10:50:20 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (36.59%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.java
@@ -6,18 +6,38 @@ class Solution {
     // diff       :[1,1,1,3,6]
     // should check the maximum value of diff less than k.
     // ans is that index+1+k
-        public int findKthPositive(int[] arr, int k) {
-        int ans=-1;
-        for(int i=0;i<arr.length;i++) {
-            int expectedNum=i+1;
-            int diff=arr[i]-expectedNum;
-            if(diff<k) {
-                ans=i;
-            }
-            else {
-                break;
-            }
-        }
-        return ans+k+1;
+    //     public int findKthPositive(int[] arr, int k) {
+    //     int ans=-1;
+    //     for(int i=0;i<arr.length;i++) {
+    //         int expectedNum=i+1;
+    //         int diff=arr[i]-expectedNum;
+    //         if(diff<k) {
+    //             ans=i;
+    //         }
+    //         else {
+    //             break;
+    //         }
+    //     }
+    //     return ans+k+1;
+    // }
+    // using binary search 
+    public int findKthPositive(int arr[],int k) {
+          int n=arr.length;
+          int si=0;
+          int ei=n-1;
+          int ans=-1;
+          while(si<=ei) {
+             int mid=(si+ei)/2;
+             int expectedNum=mid+1;
+             int diff=arr[mid]-expectedNum;
+             if(diff<k) {
+                ans=mid;
+                si=mid+1;
+             }
+             else {
+                ei=mid-1;
+             }
+          }
+          return ans+1+k;
     }
 }
