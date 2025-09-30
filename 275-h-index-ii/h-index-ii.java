da677536b2907591e379d097731cdd62d3838696
HariKrishna-png660 <htalagapu@gmail.com>
2025-09-30 17:34:29 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (92.63%) - LeetSync

--- 275-h-index-ii/h-index-ii.java
+++ 275-h-index-ii/h-index-ii.java
@@ -1,28 +1,20 @@
 class Solution {
-    // Check if h is a possible H-Index
-    public boolean isPossible(int[] citations, int h) {
-        int count = 0;
-        for (int c : citations) {
-            if (c >= h) count++;
-        }
-        return count >= h;
-    }
-
     public int hIndex(int[] citations) {
-        int n = citations.length;
-        int left = 0, right = n;
-        int maxH = 0;
-
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (isPossible(citations, mid)) {
-                maxH = mid;      // mid is possible, try higher
-                left = mid + 1;
-            } else {
-                right = mid - 1; // mid not possible, try lower
+        int n=citations.length;
+        int low=0;
+        int high=n-1;
+        while(low<=high) {
+            int mid=(low+high)/2;
+            if(citations[mid]==(n-mid)) {
+                return citations[mid];
+            }
+            else if(citations[mid]>(n-mid)) {
+                high=mid-1;
+            }
+            else {
+                low=mid+1;
             }
         }
-
-        return maxH;
+        return n-low;
     }
 }
