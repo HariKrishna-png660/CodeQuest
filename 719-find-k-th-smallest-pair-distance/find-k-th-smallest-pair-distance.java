d5cb5f6187ce50c0c82fea3e8754378533f150a9
HariKrishna-png660 <htalagapu@gmail.com>
2025-09-30 19:41:27 +0530

    Time: 7 ms (81.97%) | Memory: 44.5 MB (68.48%) - LeetSync

--- 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.java
+++ 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.java
@@ -28,27 +28,57 @@ class Solution {
     //     return pq.remove();
     // }
     // approach 3 
-    public int smallestDistancePair(int nums[],int k) {
-        int max=Integer.MIN_VALUE;
-        int n=nums.length;
-        for(int i=0;i<n;i++) {
-            max=Math.max(max,nums[i]);
-        }
-        int dis[]=new int[max+1];
-        for(int i=0;i<n;i++) {
-            for(int j=i+1;j<n;j++) {
-                int diff=Math.abs(nums[i]-nums[j]);
-                dis[diff]++;
+    // public int smallestDistancePair(int nums[],int k) {
+    //     int max=Integer.MIN_VALUE;
+    //     int n=nums.length;
+    //     for(int i=0;i<n;i++) {
+    //         max=Math.max(max,nums[i]);
+    //     }
+    //     int dis[]=new int[max+1];
+    //     for(int i=0;i<n;i++) {
+    //         for(int j=i+1;j<n;j++) {
+    //             int diff=Math.abs(nums[i]-nums[j]);
+    //             dis[diff]++;
+    //         }
+    //     }
+    //     int ans=0;
+    //     for(int i=0;i<=max;i++) {
+    //          k-=dis[i];
+    //          if(k<=0) {
+    //             ans=i;
+    //             break;
+    //          }
+    //     }
+    //     return ans;
+    // }
+    // OPTIMAL APPROACH (SLIDING WINDOW + BINARY SEARCH)
+    public boolean isPossible(int nums[],int maxDis,int k){
+        int count=0;
+        int left=0;
+        for(int right=0;right<nums.length;right++) {
+            while(nums[right]-nums[left]>maxDis) {
+                left++;
             }
+            count += right-left;
         }
-        int ans=0;
-        for(int i=0;i<=max;i++) {
-             k-=dis[i];
-             if(k<=0) {
-                ans=i;
-                break;
-             }
-        }
-        return ans;
+        return count>=k;
     }
+     public int smallestDistancePair(int nums[],int k) {
+         Arrays.sort(nums);
+         int n=nums.length;
+         int low=0;
+         int high=nums[n-1]-nums[0];
+         int dis=0;
+         while(low<=high) {
+            int mid=(low+high)/2;
+            if(isPossible(nums,mid,k)) {
+                dis=mid;
+                high=mid-1;
+            }
+            else {
+                low=mid+1;
+            }
+         }
+         return dis;
+     }
 }
