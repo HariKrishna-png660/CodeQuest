7ab63d5a1baf156878e497efd0a21cc6a8e0c06a
HariKrishna-png660 <htalagapu@gmail.com>
2025-10-01 23:28:22 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (17.39%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -14,12 +14,43 @@
  * }
  */
 class Solution {
+    // approach 1
+    // time complexity is O(n)
+    // public int countNodes(TreeNode root) {
+    //     if(root==null) {
+    //         return 0;
+    //     }
+    //     int nodesInLeftSubTree=countNodes(root.left);
+    //     int nodesInRightSubTree=countNodes(root.right);
+    //     return nodesInLeftSubTree+nodesInRightSubTree+1;
+    // }
+    // approach 2
+    // time complexity is O(logn)^2
+    public int leftHeight(TreeNode root) {
+        int height=0;
+        while(root!=null) {
+            height++;
+            root=root.left;
+        }
+        return height;
+    }
+    public int rightHeight(TreeNode root) {
+        int height=0;
+        while(root!=null) {
+            height++;
+            root=root.right;
+        }
+        return height;
+    }
     public int countNodes(TreeNode root) {
         if(root==null) {
             return 0;
         }
-        int nodesInLeftSubTree=countNodes(root.left);
-        int nodesInRightSubTree=countNodes(root.right);
-        return nodesInLeftSubTree+nodesInRightSubTree+1;
+        int lh=leftHeight(root);
+        int rh=rightHeight(root);
+        if(lh==rh) {
+            return (int)(Math.pow(2,lh)-1);
+        }
+        return countNodes(root.left)+countNodes(root.right)+1;
     }
 }
