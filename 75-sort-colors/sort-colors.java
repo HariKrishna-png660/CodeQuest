2c5c8a03b61428c3941d5ddb39e8b99990833365
HariKrishna-png660 <htalagapu@gmail.com>
2025-10-02 22:40:06 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (48.28%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -5,23 +5,17 @@ class Solution {
         arr[j]=temp;
     }
     public void sortColors(int[] nums) {
-        // we need four areas here 
-        // 0 to p1 -> zeros
-        // p1+1 to p2-1 -> ones
-        // p2 to p3 -> unexplored
-        // p3+1 to n-1 -> twos
         int n=nums.length;
         int p1=-1;
         int p2=0;
         int p3=n-1;
         while(p2<=p3) {
-            int val=nums[p2];
-            if(val==0) {
+            if(nums[p2]==0) {
+                swap(nums,p1+1,p2);
                 p1++;
-                swap(nums,p2,p1);
                 p2++;
             }
-            else if(val==1) {
+            else if(nums[p2]==1) {
                 p2++;
             }
             else {
