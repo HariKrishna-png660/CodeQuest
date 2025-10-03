041d1545914c8613c7abb9e98d528a51e35ec6f8
HariKrishna-png660 <htalagapu@gmail.com>
2025-10-03 12:49:21 +0530

    Time: 7 ms (94.40%) | Memory: 47.8 MB (34.68%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -1,4 +1,21 @@
 class Solution {
+    // for the test case [1,2,1,2,3] and k=2 
+    // window can detect the following subarrays
+    // [1] [1,2] [1,2,1] [1,2,1] [1,2,1,2] [2,3]
+    // all subarrays with atmost k differnt integers for a given subarray is length of the window
+    // all the subarrays with atmost 2 differnt integers
+    //  for [1] -> [1]
+    //  for [1,2] ->[1,2] , [2]
+    //  for [1,2,1] -> [1,2,1] , [2,1] ,[1]
+    //  for [1,2,1,2] -> [1,2,1,2] , [2,1,2] ,[1,2] ,[2]
+    //  for [2,3] -> [2,3] , [3]
+    // count=12
+    // all the subarrays with atmost 1 different integer
+    // window can detect the following subarrays
+    // [1] [2] [1] [2] [3]
+    // count=5
+    // subarrays with exact k integers = atmost k - atmost k-1
+    // ans=[1,2] [1,2,1] [2,1] [1,2,1,2] [2,1,2] [1,2] [2,3] 
     public int subarraysWithKDistinct(int[] nums, int k) {
         return subarraysWithAtMost(nums,k)-subarraysWithAtMost(nums,k-1);
     }
