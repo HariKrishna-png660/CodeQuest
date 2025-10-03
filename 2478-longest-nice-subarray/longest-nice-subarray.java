136ca3ee9fdf17e07fa3b81638d513fc4c3a25a5
HariKrishna-png660 <htalagapu@gmail.com>
2025-10-03 15:49:12 +0530

    Time: 4 ms (22.01%) | Memory: 55.8 MB (60.95%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.java
+++ 2478-longest-nice-subarray/longest-nice-subarray.java
@@ -1,39 +1,61 @@
 class Solution {
-    // maximise the minimum
-    public boolean isPossible(int nums[],int maxLength) {
-        int n=nums.length; // time complexity is O(n^2)
-        int length=1;
+    // =====================BRUTE FORCE APPROACHES======================================================================================
+    // ONLY BITMASK
+    // TIME COMPLEXITY IS O(N^2)
+    public int longestNiceSubarray(int nums[]) {
+        int n=nums.length;
+        int maxLength=1;
         for(int i=0;i<n;i++) {
             int mask=0;
             for(int j=i;j<n;j++) {
                 if((mask & nums[j])==0) {
                     mask=mask|nums[j];
-                    if(j-i+1==maxLength) {
-                        return true;
-                    }
+                    maxLength=Math.max(maxLength,j-i+1);
                 }
                 else {
                     break;
                 }
             }
         }
-        return false;
+        return maxLength;
     }
-    public int longestNiceSubarray(int[] nums) {
-        int n=nums.length;
-        int si=1;
-        int ei=n;
-        int ans=0;
-        while(si<=ei) {
-            int mid=(si+ei)/2;
-            if(isPossible(nums,mid)) {
-                ans=mid;
-                si=mid+1;
-            }
-            else {
-                ei=mid-1;
-            }
-        }
-        return ans;
-    }
+    // ====================(BINARY SEARCH ON THE ANSWER + SLIDING WINDOW) ==============================================================
+    // TIME COMPLEXITY IS O(N^2 LOGN)
+    // // maximise the minimum
+    // public boolean isPossible(int nums[],int maxLength) {
+    //     int n=nums.length; // time complexity is O(n^2)
+    //     int length=1;
+    //     for(int i=0;i<n;i++) {
+    //         int mask=0;
+    //         for(int j=i;j<n;j++) {
+    //             if((mask & nums[j])==0) {
+    //                 mask=mask|nums[j];
+    //                 if(j-i+1==maxLength) {
+    //                     return true;
+    //                 }
+    //             }
+    //             else {
+    //                 break;
+    //             }
+    //         }
+    //     }
+    //     return false;
+    // }
+    // public int longestNiceSubarray(int[] nums) {
+    //     int n=nums.length;
+    //     int si=1;
+    //     int ei=n;
+    //     int ans=0;
+    //     while(si<=ei) {
+    //         int mid=(si+ei)/2;
+    //         if(isPossible(nums,mid)) {
+    //             ans=mid;
+    //             si=mid+1;
+    //         }
+    //         else {
+    //             ei=mid-1;
+    //         }
+    //     }
+    //     return ans;
+    // }
 }
