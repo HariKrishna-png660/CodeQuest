8d7448ae7aad444a36f93438d33eb445de95d7dc
HariKrishna-png660 <htalagapu@gmail.com>
2025-10-04 04:21:57 +0530

    Time: 3 ms (91.91%) | Memory: 44.7 MB (72.06%) - LeetSync

--- 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.java
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.java
@@ -32,51 +32,86 @@ class Solution {
     //     }
     //     return ans==0?-1:ans;
     // }
-    class Pair {
-        char ch;
-        int count;
-        public Pair(char ch,int count) {
-            this.ch=ch;
-            this.count=count;
-        }
-        @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Pair)) return false;
-        Pair p = (Pair) o;
-        return this.ch == p.ch && this.count == p.count;
-    }
+    // ===============================
+    // TIME COMPLEXITY IS O(N^2)
+    // class Pair {
+    //     char ch;
+    //     int count;
+    //     public Pair(char ch,int count) {
+    //         this.ch=ch;
+    //         this.count=count;
+    //     }
+    //     @Override
+    // public boolean equals(Object o) {
+    //     if (this == o) return true;
+    //     if (!(o instanceof Pair)) return false;
+    //     Pair p = (Pair) o;
+    //     return this.ch == p.ch && this.count == p.count;
+    // }
 
-    @Override
-    public int hashCode() {
-        return 31 * ch + count;
-    }
-    }
+    // @Override
+    // public int hashCode() {
+    //     return 31 * ch + count;
+    // }
+    // }
+    // public int maximumLength(String s) {
+    //    int n=s.length();
+    //    HashMap<Pair,Integer> map=new HashMap<>();
+    //    for(int i=0;i<n;i++) {
+    //        StringBuilder sb=new StringBuilder();
+    //        for(int j=i;j<n;j++) {
+    //           if(sb.length()==0 || sb.charAt(sb.length()-1)==s.charAt(j)) {
+    //             sb.append(s.charAt(j));
+    //             int count=sb.length();
+    //             char ch=sb.charAt(0);
+    //             Pair p=new Pair(ch,count);
+    //             map.put(p,map.getOrDefault(p,0)+1);
+    //           }
+    //           else {
+    //             break;
+    //           }
+    //        }
+    //    }
+    //    int ans=0;
+    //    for(Pair key:map.keySet()) {
+    //        int val=map.get(key);
+    //        if(val>=3) {
+    //            ans=Math.max(ans,key.count);
+    //        }
+    //    }
+    //    return ans==0?-1:ans;
+    // }
+    // ===================OPTIMISED APPROACH=======================================================
+    // TIME COMPLEXITY IS O(N)
+    // SPACE COMPLEXITY IS O(26*N)
     public int maximumLength(String s) {
-       int n=s.length();
-       HashMap<Pair,Integer> map=new HashMap<>();
-       for(int i=0;i<n;i++) {
-           StringBuilder sb=new StringBuilder();
-           for(int j=i;j<n;j++) {
-              if(sb.length()==0 || sb.charAt(sb.length()-1)==s.charAt(j)) {
-                sb.append(s.charAt(j));
-                int count=sb.length();
-                char ch=sb.charAt(0);
-                Pair p=new Pair(ch,count);
-                map.put(p,map.getOrDefault(p,0)+1);
-              }
-              else {
-                break;
-              }
+        int n=s.length();
+        int matrix[][]=new int[26][n+1];
+        char prevChar=s.charAt(0);
+        int length=0;
+        for(int i=0;i<n;i++) {
+            char currChar=s.charAt(i);
+            if(prevChar==currChar) {
+                length+=1;
+                matrix[currChar-'a'][length]++;
+            }
+            else {
+                length=1;
+                matrix[currChar-'a'][length]++;
+                prevChar=currChar;
+            }
+        }
+        int result=-1;
+        for(int i=0;i<26;i++){
+            int currSum=0;
+           for(int j=n;j>=0;j--) {
+               currSum += matrix[i][j];
+               if(currSum>=3) {
+                  result =Math.max(result,j);
+                  break;
+               }
            }
-       }
-       int ans=0;
-       for(Pair key:map.keySet()) {
-           int val=map.get(key);
-           if(val>=3) {
-               ans=Math.max(ans,key.count);
-           }
-       }
-       return ans==0?-1:ans;
+        }
+        return result;
     }
 }
