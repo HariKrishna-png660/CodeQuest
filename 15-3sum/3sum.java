1f1fd2283b2214a4e25a227397a9f64acc6e7fc0
HariKrishna-png660 <htalagapu@gmail.com>
2025-10-05 09:10:25 +0530

    Time: 30 ms (71.17%) | Memory: 52.8 MB (15.08%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,62 +1,55 @@
 class Solution {
-    public List<List<Integer>> makePairs(int[] arr, int target, int si, int ei) {
-        int i=si; int j=ei;
-        List<List<Integer>> ans = new ArrayList<>();
-        while(i < j){
-            int sum = arr[i] + arr[j];
+    public List<List<Integer>> makePairs(int nums[],int target,int si,int ei) {
+        int i=si;
+        int j=ei;
+        List<List<Integer>> ans=new ArrayList<>();
+        while(i<j) {
+            int sum=nums[i]+nums[j];
+            if(sum==target) {
+               List<Integer> smallAns=new ArrayList<>();
+               smallAns.add(nums[i]);
+               smallAns.add(nums[j]);
+               ans.add(smallAns);
+               i++;
+               j--;
 
-            if(sum == target){
-                ArrayList<Integer> smallAns = new ArrayList<>();
-                smallAns.add(arr[i]);
-                smallAns.add(arr[j]);
-                ans.add(smallAns);
-                // ans.add(Arrays.asList(arr[i],arr[j]));
+               while(i<j && nums[i-1]==nums[i]) i++;
+               while(i<j && nums[j+1]==nums[j]) j--; 
+            }
+            else if(sum<target) {
                 i++;
+            }
+            else {
                 j--;
-
-                // skipping over already seen elements to remove duplicate pairs
-                while(i<j && arr[i-1] == arr[i]) i++;
-                while(i<j && arr[j+1] == arr[j]) j--;
-            } else if(sum < target){
-                i++;
-            } else {
-                j--;
-            }            
+            }
         }
-
         return ans;
     }
-    
-    // generic function to get triplets
-    public List<List<Integer>> threesum(int[] nums, int target, int si, int ei){
-        List<List<Integer>> ans = new ArrayList<>();
-        
-        for(int i=si; i<=ei; i++){
-            // removing duplicate elements
-            if(i>si && nums[i-1] == nums[i]) continue;
-
-            int fixedElement = nums[i];
-
-            int updatedTarget = target - fixedElement;
-
-            // returns all two sum pairs
-            List<List<Integer>> smallAns = makePairs(nums,updatedTarget,i+1,ei);
-            addFixedElement(ans,smallAns,fixedElement);
-        }
-
-        return ans;
-    }
-
-    public  void addFixedElement(List<List<Integer>> ans, List<List<Integer>> smallAns, int fixedElement){
-        for(List<Integer> sAns: smallAns){
+    public void addFixedElement(List<List<Integer>> ans,List<List<Integer>> smallAns,int fixedElement) {
+        for(List<Integer> sAns:smallAns) {
             sAns.add(fixedElement);
             ans.add(sAns);
         }
     }
+    public List<List<Integer>> kSum(int nums[],int target,int k,int si,int ei) {
+        if(k==2) {
+            return makePairs(nums,target,si,ei);
+        }
+        List<List<Integer>> ans=new ArrayList<>();
 
+        for(int i=si;i<=ei;i++) {
+            if(i>si && nums[i-1]==nums[i]) {
+                continue;
+            }
+            int fixedElement=nums[i];
+            int updatedTarget=target-fixedElement;
+            List<List<Integer>> smallAns=kSum(nums,updatedTarget,k-1,i+1,ei);
+            addFixedElement(ans,smallAns,fixedElement);
+        }
+        return ans;
+    }
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-
-        return threesum(nums,0,0,nums.length-1);
+        return kSum(nums,0,3,0,nums.length-1);
     }
 }
