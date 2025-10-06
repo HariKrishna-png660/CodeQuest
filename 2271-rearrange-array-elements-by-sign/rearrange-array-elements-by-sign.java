daccfeb6b105cfe07e400fddb71815b96bf25a14
HariKrishna-png660 <htalagapu@gmail.com>
2025-10-06 18:45:45 +0530

    Time: 3 ms (100.00%) | Memory: 76.7 MB (86.89%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,24 +1,41 @@
 class Solution {
-    public int[] rearrangeArray(int[] nums) {
+    // public int[] rearrangeArray(int[] nums) {
+    //     int n=nums.length;
+    //     int arr[]=new int[n];
+    //     int p=0;
+    //     int q=n/2;
+    //     for(int i=0;i<nums.length;i++) {
+    //         if(nums[i]>0) {
+    //             arr[p++]=nums[i];
+    //         }
+    //         else {
+    //             arr[q++]=nums[i];
+    //         }
+    //     }
+    //     p=0;
+    //     q=n/2;
+    //     int i=0;
+    //     while(i<n) {
+    //         nums[i++]=arr[p++];
+    //         nums[i++]=arr[q++];
+    //     }
+    //     return nums;
+    // }
+    public int[] rearrangeArray(int nums[]) {
         int n=nums.length;
         int arr[]=new int[n];
-        int p=0;
-        int q=n/2;
-        for(int i=0;i<nums.length;i++) {
+        int pi=0;
+        int ni=1;
+        for(int i=0;i<n;i++) {
             if(nums[i]>0) {
-                arr[p++]=nums[i];
+                arr[pi]=nums[i];
+                pi+=2;
             }
             else {
-                arr[q++]=nums[i];
+                arr[ni]=nums[i];
+                ni += 2;
             }
         }
-        p=0;
-        q=n/2;
-        int i=0;
-        while(i<n) {
-            nums[i++]=arr[p++];
-            nums[i++]=arr[q++];
-        }
-        return nums;
+        return arr;
     }
 }
