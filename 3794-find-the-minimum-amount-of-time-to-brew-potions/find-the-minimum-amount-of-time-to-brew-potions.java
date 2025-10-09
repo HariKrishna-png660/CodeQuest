3d2558f75a7601721ec33915c73a082f3cce58fd
HariKrishna-png660 <htalagapu@gmail.com>
2025-10-09 05:43:55 +0530

    Time: 102 ms (84.51%) | Memory: 44.7 MB (54.93%) - LeetSync

---
+++ 3794-find-the-minimum-amount-of-time-to-brew-potions/find-the-minimum-amount-of-time-to-brew-potions.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long minTime(int[] skill, int[] mana) {
+        if (mana.length == 0) return 0L;
+        long sumSkill = 0L;
+        for (int s : skill) sumSkill += s;
+        long prevWizardDone = sumSkill * (long)mana[0];
+        for (int j = 1; j < mana.length; ++j) {
+            long prevPotionDone = prevWizardDone;
+            for (int i = skill.length - 2; i >= 0; --i) {
+                prevPotionDone -= (long)skill[i + 1] * (long)mana[j - 1];
+                prevWizardDone = Math.max(prevPotionDone, prevWizardDone - (long)skill[i] * (long)mana[j]);
+            }
+            prevWizardDone += sumSkill * (long)mana[j];
+        }
+        return prevWizardDone;
+    }
+}
