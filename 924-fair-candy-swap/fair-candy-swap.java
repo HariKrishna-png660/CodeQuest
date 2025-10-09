c38bd939a9873a0db7c10ccf64c1d9c03ced0891
HariKrishna-png660 <htalagapu@gmail.com>
2025-10-09 06:22:46 +0530

    Time: 8 ms (98.00%) | Memory: 46.2 MB (20.97%) - LeetSync

--- 924-fair-candy-swap/fair-candy-swap.java
+++ 924-fair-candy-swap/fair-candy-swap.java
@@ -1,5 +1,26 @@
 class Solution {
-    public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
+    // public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
+    //     int n=aliceSizes.length;
+    //     int m=bobSizes.length;
+    //     int sumA=0;
+    //     int sumB=0;
+    //     for(int i=0;i<n;i++) {
+    //         sumA += aliceSizes[i];
+    //     }
+    //     for(int j=0;j<m;j++) {
+    //         sumB += bobSizes[j];
+    //     }
+    //     for(int i=0;i<n;i++) {
+    //         for(int j=0;j<m;j++) {
+    //             if(sumA-aliceSizes[i]+bobSizes[j]==sumB+aliceSizes[i]-bobSizes[j]) {
+    //                 return new int[]{aliceSizes[i],bobSizes[j]};
+    //             }
+    //         }
+    //     }
+    //     return new int[]{};
+    // }
+    public int[] fairCandySwap(int aliceSizes[],int bobSizes[]) {
+        Set<Integer> setB=new HashSet<>();
         int n=aliceSizes.length;
         int m=bobSizes.length;
         int sumA=0;
@@ -7,14 +28,14 @@ class Solution {
         for(int i=0;i<n;i++) {
             sumA += aliceSizes[i];
         }
-        for(int j=0;j<m;j++) {
-            sumB += bobSizes[j];
+        for(int i=0;i<m;i++) {
+            sumB += bobSizes[i];
+            setB.add(bobSizes[i]);
         }
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<m;j++) {
-                if(sumA-aliceSizes[i]+bobSizes[j]==sumB+aliceSizes[i]-bobSizes[j]) {
-                    return new int[]{aliceSizes[i],bobSizes[j]};
-                }
+        int diff=(sumB-sumA)/2;
+        for(int val:aliceSizes) {
+            if(setB.contains(val+diff)) {
+                return new int[]{val,val+diff};
             }
         }
         return new int[]{};
