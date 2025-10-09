39915e829a065724b5e574d9f4fbbecb3d2982eb
HariKrishna-png660 <htalagapu@gmail.com>
2025-10-09 08:19:47 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (82.53%) - LeetSync

--- 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.java
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.java
@@ -1,52 +1,70 @@
 class Solution {
-    public int getFirstIndex(int nums[],int target) {
-          int ans=-1;
-          int si=0;
-          int ei=nums.length-1;
-          while(si<=ei) {
-             int mid=(si+ei)/2;
-             if(nums[mid]==target) {
-                ans=mid;
-                ei=mid-1;
-             }
-             else if(nums[mid]<target) {
-                si=mid+1;
-             }
-             else {
-               ei=mid-1;
-             }
-          }
-          return ans;
-    }
-    public int getLastIndex(int nums[],int target) {
-          int ans=-1;
-          int si=0;
-          int ei=nums.length-1;
-          while(si<=ei) {
-              int mid=(si+ei)/2;
-              if(nums[mid]==target) {
-                   ans=mid;
-                   si=mid+1;
-              }
-              else if(nums[mid]<target) {
-                   si=mid+1;
-              }
-              else {
-                   ei=mid-1;
-              }
-          }
-          return ans;
-    }
-    public List<Integer> targetIndices(int[] nums, int target) {
-        Arrays.sort(nums);
-        int firstIndex=getFirstIndex(nums,target);
-        int lastIndex=getLastIndex(nums,target);
-        List<Integer> ans=new ArrayList<>();
-        if(firstIndex==-1) {
-            return new ArrayList<>();
+    // public int getFirstIndex(int nums[],int target) {
+    //       int ans=-1;
+    //       int si=0;
+    //       int ei=nums.length-1;
+    //       while(si<=ei) {
+    //          int mid=(si+ei)/2;
+    //          if(nums[mid]==target) {
+    //             ans=mid;
+    //             ei=mid-1;
+    //          }
+    //          else if(nums[mid]<target) {
+    //             si=mid+1;
+    //          }
+    //          else {
+    //            ei=mid-1;
+    //          }
+    //       }
+    //       return ans;
+    // }
+    // public int getLastIndex(int nums[],int target) {
+    //       int ans=-1;
+    //       int si=0;
+    //       int ei=nums.length-1;
+    //       while(si<=ei) {
+    //           int mid=(si+ei)/2;
+    //           if(nums[mid]==target) {
+    //                ans=mid;
+    //                si=mid+1;
+    //           }
+    //           else if(nums[mid]<target) {
+    //                si=mid+1;
+    //           }
+    //           else {
+    //                ei=mid-1;
+    //           }
+    //       }
+    //       return ans;
+    // }
+    // public List<Integer> targetIndices(int[] nums, int target) {
+    //     Arrays.sort(nums);
+    //     int firstIndex=getFirstIndex(nums,target);
+    //     int lastIndex=getLastIndex(nums,target);
+    //     List<Integer> ans=new ArrayList<>();
+    //     if(firstIndex==-1) {
+    //         return new ArrayList<>();
+    //     }
+    //     for(int i=firstIndex;i<=lastIndex;i++) {
+    //         ans.add(i);
+    //     }
+    //     return ans;
+    // }
+    public List<Integer> targetIndices(int[] nums, int target) { 
+        int n=nums.length;
+        int less=0;
+        int equal=0;
+        for(int i=0;i<n;i++) {
+            if(nums[i]<target) {
+                less++;
+            }
+            else if(nums[i]==target) {
+                equal++;
+            }
         }
-        for(int i=firstIndex;i<=lastIndex;i++) {
-            ans.add(i);
+        List<Integer> ans=new ArrayList<>();
+        for(int i=0;i<equal;i++) {
+            ans.add(less+i);
         }
         return ans;
     }
