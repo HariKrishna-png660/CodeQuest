ae3e7fb5e11f161d283871477552290c27d7700c
HariKrishna-png660 <htalagapu@gmail.com>
2025-10-11 14:59:33 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (67.50%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -3,21 +3,18 @@ class Solution {
         int n=nums.length;
         int si=0;
         int ei=n-1;
-        if(target<nums[0]) {
-            return 0;
-        }
-        if(target>nums[n-1]) {
-            return n;
-        }
-        while(si<ei) {
+        while(si<=ei) {
             int mid=(si+ei)/2;
-            if(nums[mid]<target) {
+            if(nums[mid]==target) {
+                return mid;
+            }
+            else if(nums[mid]<target) {
                 si=mid+1;
             }
             else {
-                ei=mid;
+                ei=mid-1;
             }
         }
-        return si; // return ei also will work......
+        return si;
     }
 }
