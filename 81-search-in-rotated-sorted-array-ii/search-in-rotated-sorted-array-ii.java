322ec5c42fd1f28cabd52f730019c0afd6b342ee
HariKrishna-png660 <htalagapu@gmail.com>
2025-10-12 05:19:03 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (83.51%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -2,16 +2,47 @@ class Solution {
     // code written for leetcode 33 
     // will fail for test case 
     // [1,0,1,1,1]
-    public boolean search(int[] nums, int target) {
+    // public boolean search(int[] nums, int target) {
+    //     int n=nums.length;
+    //     int si=0;
+    //     int ei=n-1;
+    //     while(si<=ei) {   // time complexity is average case O(log n)  worst case O(n)
+    //         int mid=(si+ei)/2;
+    //         if(nums[mid]==target || nums[si]==target) {
+    //              return true;
+    //         }
+    //         else if(nums[si]<nums[mid]) { // left part is sorted 
+    //             if(nums[si]<=target && nums[mid]>target) {
+    //                 ei=mid-1;
+    //             }
+    //             else {
+    //                 si=mid+1;
+    //             }
+    //         }
+    //         else if(nums[mid]<nums[ei]) { // right part is sorted 
+    //             if(nums[mid]<target && nums[ei]>=target) {
+    //                 si=mid+1;
+    //             }
+    //             else {
+    //                 ei=mid-1;
+    //             }
+    //         }
+    //         else {  // either si and mid are coinciding or area is not sorted 
+    //             si++;
+    //         }
+    //     }
+    //     return false;
+    // }
+     public boolean search(int[] nums, int target) {
         int n=nums.length;
         int si=0;
         int ei=n-1;
-        while(si<=ei) {   // time complexity is average case O(log n)  worst case O(n)
+        while(si<=ei) {
             int mid=(si+ei)/2;
             if(nums[mid]==target || nums[si]==target) {
-                 return true;
+                return true;
             }
-            else if(nums[si]<nums[mid]) { // left part is sorted 
+            else if(nums[si]<nums[ei]) {
                 if(nums[si]<=target && nums[mid]>target) {
                     ei=mid-1;
                 }
@@ -19,18 +50,18 @@ class Solution {
                     si=mid+1;
                 }
             }
-            else if(nums[mid]<nums[ei]) { // right part is sorted 
+            else if(nums[mid]<nums[ei]) {
                 if(nums[mid]<target && nums[ei]>=target) {
                     si=mid+1;
                 }
                 else {
-                    ei=mid-1;
+                    ei=mid-1;  
                 }
             }
-            else {  // either si and mid are coinciding or area is not sorted 
+            else {
                 si++;
             }
-        }
-        return false;
-    }
+         }
+         return false;
+     }
 }
