dd51a13cf8b855483126c4eb27aa57ba8add368d
HariKrishna-png660 <htalagapu@gmail.com>
2025-10-11 08:50:43 +0530

    Time: 20 ms (77.07%) | Memory: 56.9 MB (24.74%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,4 +1,5 @@
 class Solution {
+    // MERGE SORT IMPLEMENTATION 
     public int[] mergeSortedArrays(int a[],int b[]) {
         int n1=a.length;
         int n2=b.length;
@@ -37,4 +38,36 @@ class Solution {
         int n=nums.length;
         return mergeSort(nums,0,n-1);
     }
+    // QUICK SORT IMPLEMENTATION =====
+    // public void swap(int i,int j,int arr[]) {
+    //     int temp=arr[i];
+    //     arr[i]=arr[j];
+    //     arr[j]=temp;
+    // }
+    // public int partition(int nums[],int si,int ei) {
+    //     int pivot=nums[ei];
+    //     int i=si;int j=si;
+    //     while(j<=ei-1) {
+    //         if(nums[j]<=pivot) {
+    //             swap(i,j,nums);
+    //             i++;
+    //         }
+    //         j++;
+    //     }
+    //     swap(i,ei,nums);
+    //     return i;
+    // }
+    // public void quickSort(int nums[],int si,int ei) {
+    //     if(si>ei) {
+    //         return;
+    //     }
+    //     int pivotIndex=partition(nums,si,ei);
+    //     quickSort(nums,si,pivotIndex-1);
+    //     quickSort(nums,pivotIndex+1,ei);
+    // }
+    // public int[] sortArray(int[] nums) {
+    //     int n=nums.length;
+    //      quickSort(nums,0,n-1);
+    //      return nums;
+    // }
 }
