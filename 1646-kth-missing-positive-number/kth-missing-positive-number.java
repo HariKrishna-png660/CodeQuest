9a7d99484e4c237f4f3e4c4f1927d4749213505d
HariKrishna-png660 <htalagapu@gmail.com>
2025-10-11 16:12:15 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (27.34%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.java
@@ -21,23 +21,42 @@ class Solution {
     //     return ans+k+1;
     // }
     // using binary search 
-    public int findKthPositive(int arr[],int k) {
-          int n=arr.length;
-          int si=0;
-          int ei=n-1;
-          int ans=-1;
-          while(si<=ei) {  // time complexity is O(log n)
-             int mid=(si+ei)/2;
-             int expectedNum=mid+1;
-             int diff=arr[mid]-expectedNum;
-             if(diff<k) {
-                ans=mid;
-                si=mid+1;
-             }
-             else {
-                ei=mid-1;
-             }
-          }
-          return ans+1+k;
-    }
+    // public int findKthPositive(int arr[],int k) {
+    //       int n=arr.length;
+    //       int si=0;
+    //       int ei=n-1;
+    //       int ans=-1;
+    //       while(si<=ei) {  // time complexity is O(log n)
+    //          int mid=(si+ei)/2;
+    //          int expectedNum=mid+1;
+    //          int diff=arr[mid]-expectedNum;
+    //          if(diff<k) {
+    //             ans=mid;
+    //             si=mid+1;
+    //          }
+    //          else {
+    //             ei=mid-1;
+    //          }
+    //       }
+    //       return ans+1+k;
+    // }
+      public int findKthPositive(int arr[],int k) {
+            int n=arr.length;
+            int si=0;
+            int ei=n-1;
+            int ans=-1;
+            while(si<=ei) {
+                int mid=(si+ei)/2;
+                int expectedNum=(mid+1);
+                int diff=arr[mid]-expectedNum;
+                if(diff<k) {
+                    ans=mid;
+                    si=mid+1;
+                }
+                else {
+                    ei=mid-1;
+                }
+            }
+            return ans+1+k;
+      }
 }
