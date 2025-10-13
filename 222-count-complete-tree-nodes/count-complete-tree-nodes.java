1c4df015b38ffb2ef55ae67a92234a0abc48a7a4
HariKrishna-png660 <htalagapu@gmail.com>
2025-10-13 23:41:13 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (51.46%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -26,23 +26,50 @@ class Solution {
     // }
     // approach 2
     // time complexity is O(logn)^2
-    public int leftHeight(TreeNode root) {
-        int height=0;
-        while(root!=null) {
+    // public int leftHeight(TreeNode root) {
+    //     int height=0;
+    //     while(root!=null) {
+    //         height++;
+    //         root=root.left;
+    //     }
+    //     return height;
+    // }
+    // public int rightHeight(TreeNode root) {
+    //     int height=0;
+    //     while(root!=null) {
+    //         height++;
+    //         root=root.right;
+    //     }
+    //     return height;
+    // }
+    // public int countNodes(TreeNode root) {
+    //     if(root==null) {
+    //         return 0;
+    //     }
+    //     int lh=leftHeight(root);
+    //     int rh=rightHeight(root);
+    //     if(lh==rh) {
+    //         return (int)(Math.pow(2,lh)-1);
+    //     }
+    //     return countNodes(root.left)+countNodes(root.right)+1;
+    // }
+     public int leftHeight(TreeNode root) {
+         int height=0;
+         while(root!=null) {
             height++;
             root=root.left;
-        }
-        return height;
-    }
-    public int rightHeight(TreeNode root) {
+         }
+         return height;
+     }
+     public int rightHeight(TreeNode root) {
         int height=0;
         while(root!=null) {
             height++;
             root=root.right;
         }
         return height;
-    }
-    public int countNodes(TreeNode root) {
+     }
+     public int countNodes(TreeNode root) {
         if(root==null) {
             return 0;
         }
@@ -52,5 +79,5 @@ class Solution {
             return (int)(Math.pow(2,lh)-1);
         }
         return countNodes(root.left)+countNodes(root.right)+1;
-    }
+     }
 }
