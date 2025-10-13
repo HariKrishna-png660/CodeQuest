f77992e61e716db8e75cdc3a00549421cb6dc77f
HariKrishna-png660 <htalagapu@gmail.com>
2025-10-13 18:34:27 +0530

    Time: 18 ms (66.88%) | Memory: 58.4 MB (57.67%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,38 +1,71 @@
 class Solution {
-    // this is one of the problem in which we doing binary search on the answer
-    // this is a problem in which we need to minimize the maximum
-    public boolean isPossible(int bloomDay[],int min,int m,int k) {
+    // // this is one of the problem in which we doing binary search on the answer
+    // // this is a problem in which we need to minimize the maximum
+    // public boolean isPossible(int bloomDay[],int min,int m,int k) {
+    //     int n=bloomDay.length;
+    //     int count=0;
+    //     for(int i=0;i<n;i++) {
+    //         int val=bloomDay[i];
+    //         if(val<=min) {
+    //             count++;
+    //             if(count==k) {
+    //                 m--;
+    //                 count=0;
+    //             }
+    //         }
+    //         else {
+    //             count=0;
+    //         }
+    //     }
+    //     return m<=0;
+    // }
+    // public int minDays(int[] bloomDay, int m, int k) {
+    //     int si=1;
+    //     int ei=(int)(1e9);
+    //     int minDay=-1;
+    //     while(si<=ei) {      // time complexity is O(n log n)
+    //         int mid=(si+ei)/2;
+    //         if(isPossible(bloomDay,mid,m,k)) {
+    //             minDay=mid;
+    //             ei=mid-1;
+    //         }
+    //         else {
+    //             si=mid+1;
+    //         }
+    //     }
+    //     return minDay;
+    // }
+    public boolean isPossible(int bloomDay[],int minDay,int k,int m) {
         int n=bloomDay.length;
-        int count=0;
+        int K=k;
         for(int i=0;i<n;i++) {
-            int val=bloomDay[i];
-            if(val<=min) {
-                count++;
-                if(count==k) {
-                    m--;
-                    count=0;
-                }
-            }
-            else {
-                count=0;
-            }
+             if(bloomDay[i]<=minDay) {
+                K--;
+                if(K==0) {
+                 m--;
+                 K=k;
+               }
+             }
+             else {
+                K=k;
+             }
         }
         return m<=0;
     }
     public int minDays(int[] bloomDay, int m, int k) {
-        int si=1;
-        int ei=(int)(1e9);
-        int minDay=-1;
-        while(si<=ei) {      // time complexity is O(n log n)
-            int mid=(si+ei)/2;
-            if(isPossible(bloomDay,mid,m,k)) {
-                minDay=mid;
-                ei=mid-1;
+         int low=1; // when bloomDay={1} m=1 and k=1
+         int high=(int)(1e9); // when bloomDay={10^9} m=1 and k=1
+         int ans=-1;
+         while(low<=high) {
+            int mid=(low+high)/2;
+            if(isPossible(bloomDay,mid,k,m)) {
+                ans=mid;
+                high=mid-1;
             }
             else {
-                si=mid+1;
+                low=mid+1;
             }
-        }
-        return minDay;
+         }
+         return ans;
     }
 }
