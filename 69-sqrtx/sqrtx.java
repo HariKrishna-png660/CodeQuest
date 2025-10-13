3992ad24a4cf094e0b193a587da37d37fd2a0ea3
HariKrishna-png660 <htalagapu@gmail.com>
2025-10-13 23:28:50 +0530

    Time: 1 ms (85.21%) | Memory: 41 MB (55.53%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,17 +1,19 @@
 class Solution {
-    public boolean isPossible(int x, int min) {
-        return (long)min * min <= x; 
+    public boolean isPossible(int min,int x) {
+        return (long)min*min<=x;
     }
-    
     public int mySqrt(int x) {
-        int low = 0, high = x, ans = 0;
-        while (low <= high) {
-            int mid = low + (high - low) / 2; 
-            if (isPossible(x, mid)) {
-                ans = mid;
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+        int low=1;
+        int high=x;
+        int ans=0;
+        while(low<=high) {
+            int mid=low+(high-low)/2;
+            if(isPossible(mid,x)) {
+                ans=mid;
+                low=mid+1;
+            }
+            else {
+                high=mid-1;
             }
         }
         return ans;
