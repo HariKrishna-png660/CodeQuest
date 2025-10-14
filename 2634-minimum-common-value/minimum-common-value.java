e9df5de8c0cc3d3cc0eabc48da7c1bb42b32408d
HariKrishna-png660 <htalagapu@gmail.com>
2025-10-14 18:23:05 +0530

    Time: 1 ms (97.84%) | Memory: 62.3 MB (89.75%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -1,22 +1,41 @@
 class Solution {
-    public int getCommon(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        int n=nums1.length;
-        int m=nums2.length;
-        int i=0;
-        int j=0;
-        while(i<n && j<m) {
-           if(nums1[i]==nums2[j]) {
-               return nums1[i];
-           }
-           else if(nums1[i]<nums2[j]) {
-               i++;
-           }
-           else {
-              j++;
-           }
-        }
-        return -1;
-    }
+    // public int getCommon(int[] nums1, int[] nums2) {
+    //     Arrays.sort(nums1);
+    //     Arrays.sort(nums2);
+    //     int n=nums1.length;
+    //     int m=nums2.length;
+    //     int i=0;
+    //     int j=0;
+    //     while(i<n && j<m) {
+    //        if(nums1[i]==nums2[j]) {
+    //            return nums1[i];
+    //        }
+    //        else if(nums1[i]<nums2[j]) {
+    //            i++;
+    //        }
+    //        else {
+    //           j++;
+    //        }
+    //     }
+
+    //     return -1;
+    // }
+     public int getCommon(int[] nums1, int[] nums2) {
+         int i=0;
+         int j=0;
+         int n=nums1.length;
+         int m=nums2.length;
+         while(i<n && j<m) {
+              if(nums1[i]==nums2[j]) {
+                return nums1[i];
+              }
+              else if(nums1[i]<nums2[j]) {
+                 i++;
+              }
+              else {
+                 j++;
+              }
+         }
+         return -1;
+     }
 }
