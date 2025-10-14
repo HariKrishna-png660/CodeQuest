5ed80e3a08363152b091ccb1919188d2180e1d96
HariKrishna-png660 <htalagapu@gmail.com>
2025-10-14 22:06:25 +0530

    Time: 5 ms (98.81%) | Memory: 45.3 MB (30.02%) - LeetSync

--- 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.java
+++ 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.java
@@ -1,16 +1,37 @@
 class Solution {
+    // ====TIME COMPLEXITY IS O(N*M)=========
+    // public int getMaxSize(int prefix[],int tarSum) {
+    //       int n=prefix.length;
+    //       int ans=0;
+    //       for(int i=0;i<n;i++) {
+    //            if(prefix[i]<=tarSum) {
+    //               ans=i+1;
+    //            }
+    //            else {
+    //               break;
+    //            }
+    //       }
+    //       return ans;
+    // }
     public int getMaxSize(int prefix[],int tarSum) {
-          int n=prefix.length;
-          int ans=0;
-          for(int i=0;i<n;i++) {
-               if(prefix[i]<=tarSum) {
-                  ans=i+1;
-               }
-               else {
-                  break;
-               }
-          }
-          return ans;
+        int n=prefix.length;
+        int ans=0;
+        int low=0;
+        int high=n-1;
+        if(tarSum<prefix[0]) {
+            return -1;
+        }
+        while(low<=high) {
+            int mid=(low+high)/2;
+            if(prefix[mid]<=tarSum) {
+                ans=mid;
+                low=mid+1;
+            }
+            else {
+               high=mid-1;
+            }
+        }
+        return ans;
     }
     public int[] answerQueries(int[] nums, int[] queries) {
          Arrays.sort(nums);
@@ -24,7 +45,7 @@ class Solution {
          int ans[]=new int[m];
          for(int i=0;i<m;i++) {
              int maxSize=getMaxSize(prefix,queries[i]);
-             ans[i]=maxSize;
+             ans[i]=maxSize+1;
          }
          return ans;
     }
