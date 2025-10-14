e653e4c13356256d8428c363e490917f3a3e6f8a
HariKrishna-png660 <htalagapu@gmail.com>
2025-10-14 18:00:46 +0530

    Time: 2 ms (95.67%) | Memory: 42.5 MB (53.14%) - LeetSync

--- 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.java
+++ 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.java
@@ -14,21 +14,34 @@ class Solution {
     //     return count;
     // }
     // TIME COMPLEXITY IS O(N LOG N)
+    // public int countPairs(List<Integer> nums,int target) {
+    //       Collections.sort(nums);
+    //       int i=0;
+    //       int j=nums.size()-1;
+    //       int count=0;
+    //       while(i<j) {
+    //          int sum=nums.get(i)+nums.get(j);
+    //          if(sum<target) {
+    //             count += (j-i);
+    //             i++;
+    //          }
+    //          else {
+    //             j--;
+    //          }
+    //       }
+    //       return count;
+    // }
     public int countPairs(List<Integer> nums,int target) {
-          Collections.sort(nums);
-          int i=0;
-          int j=nums.size()-1;
-          int count=0;
-          while(i<j) {
-             int sum=nums.get(i)+nums.get(j);
-             if(sum<target) {
-                count += (j-i);
-                i++;
-             }
-             else {
-                j--;
-             }
-          }
-          return count;
+        int n=nums.size();
+        int ans=0;
+        for(int i=0;i<n;i++) {
+            for(int j=i+1;j<n;j++) {
+                int sum=nums.get(i)+nums.get(j);
+                if(sum<target) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
     }
 }
