0d6cacf91b6f6dd3e65c9c9cfacbccdea1429a49
HariKrishna-png660 <htalagapu@gmail.com>
2025-10-15 13:46:10 +0530

    Time: 170 ms (7.45%) | Memory: 88.5 MB (54.26%) - LeetSync

---
+++ 3619-adjacent-increasing-subarrays-detection-ii/adjacent-increasing-subarrays-detection-ii.java
@@ -0,0 +1,41 @@
+class Solution {
+    // ==============TIME COMPLEXITY IS O(N LOG N)
+    public boolean isPossible(List<Integer> nums,int k) {
+        int n=nums.size();
+        int currRun=1;
+        int prevRun=-1;
+        for(int i=1;i<n;i++) {
+            if(nums.get(i)>nums.get(i-1)) {
+                currRun++;
+            }
+            else {
+                prevRun=currRun;
+                currRun=1;
+            }
+            if(currRun/2>=k) {
+                return true;
+            }
+            if(Math.min(prevRun,currRun)>=k) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public int maxIncreasingSubarrays(List<Integer> nums) {
+        int n=nums.size();
+        int low=1;
+        int high=(int)(1e5);
+        int ans=0;
+        while(low<=high) {
+            int mid=(low+high)/2;
+            if(isPossible(nums,mid)) {
+                 ans=mid;
+                 low=mid+1;
+            }
+            else {
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+}
