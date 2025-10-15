a62c5650dbdb14fb2be925d74e9e03212405d4bd
HariKrishna-png660 <htalagapu@gmail.com>
2025-10-15 17:15:13 +0530

    Time: 46 ms (81.31%) | Memory: 62.9 MB (5.61%) - LeetSync

--- 3944-minimum-time-to-activate-string/minimum-time-to-activate-string.java
+++ 3944-minimum-time-to-activate-string/minimum-time-to-activate-string.java
@@ -1,46 +1,42 @@
 class Solution {
-    public boolean isActiveAtTime(int t, String s, int[] order, int k) {
-        int n = s.length();
-        boolean[] isStar = new boolean[n];
-        for (int i = 0; i <= t; i++) {
-            isStar[order[i]] = true;
-        }
-
-        long invalidCount = 0;
-        long segmentLength = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (!isStar[i]) {
-                segmentLength++;
-            } else {
-                invalidCount += (segmentLength * (segmentLength + 1)) / 2;
-                segmentLength = 0;
-            }
-        }
-        if (segmentLength > 0) {
-            invalidCount += (segmentLength * (segmentLength + 1)) / 2;
-        }
-
-        long totalSubstrings = ((long) n * (n + 1)) / 2;
-        long validSubstrings = totalSubstrings - invalidCount;
-
-        return validSubstrings >= k;
+    public boolean isPossible(String s,int order[],int k,int minTime) {
+        int n=s.length();
+         int marker[]=new int[n];
+         for(int i=0;i<=minTime;i++) {
+            marker[order[i]]=1;
+         }
+         long ans=0;
+         long len=0;
+         for(int i=0;i<n;i++) {
+             if(marker[i]==0) {
+                len++;
+             }
+             else {
+                ans += (len*(len+1)/2);
+                len=0;
+             }
+         }
+         if(len>0) {
+                ans += (len*(len+1))/2;
+         }
+         long total=((long)n*(n+1))/2; 
+         long req=total-ans;
+         return req>=k;
     }
-
     public int minTime(String s, int[] order, int k) {
-        int n = s.length();
-        int left = 0, right = n - 1, answer = -1;
-
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (isActiveAtTime(mid, s, order, k)) {
-                answer = mid;
-                right = mid - 1;
-            } else {
-                left = mid + 1;
+        int low=0;
+        int high=order.length-1;
+        int ans=-1;
+        while(low<=high) {
+            int mid=(low+high)/2;
+            if(isPossible(s,order,k,mid)) {
+                ans=mid;
+                high=mid-1;
+            }
+            else {
+                low=mid+1;
             }
         }
-
-        return answer;
+        return ans;
     }
 }
