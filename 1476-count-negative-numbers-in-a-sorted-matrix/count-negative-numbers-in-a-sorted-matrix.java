46ef3bca63063334a978a1bc60d52f32c3a862ee
HariKrishna-png660 <htalagapu@gmail.com>
2025-10-15 15:15:09 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (74.67%) - LeetSync

--- 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.java
+++ 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.java
@@ -14,30 +14,48 @@ class Solution {
     //     return count;
     // }
     // ==============TIME COMPLEXITY IS O(N LOG N)=================
-    public int getNegativeCount(int arr[]) {
-        int n=arr.length;
-        int si=0;
-        int ei=n-1;
-        int ans=n;
-        while(si<=ei) {
-            int mid=(si+ei)/2;
-            if(arr[mid]<0) {
-               ans=mid;
-               ei=mid-1;
-            }
-            else {
-                si=mid+1;
-            }
-        }
-        return ans;
-    }
+    // public int getNegativeCount(int arr[]) {
+    //     int n=arr.length;
+    //     int si=0;
+    //     int ei=n-1;
+    //     int ans=n;
+    //     while(si<=ei) {
+    //         int mid=(si+ei)/2;
+    //         if(arr[mid]<0) {
+    //            ans=mid;
+    //            ei=mid-1;
+    //         }
+    //         else {
+    //             si=mid+1;
+    //         }
+    //     }
+    //     return ans;
+    // }
+    // public int countNegatives(int[][] grid) {
+    //     int n=grid.length;
+    //     int ans=0;
+    //     for(int i=0;i<n;i++) {
+    //        int negCount=getNegativeCount(grid[i]);
+    //        System.out.println(negCount);
+    //        ans += grid[i].length-negCount;
+    //     }
+    //     return ans;
+    // }
+    // ===========TIME COMPLEXITY IS O(N+M)=============
     public int countNegatives(int[][] grid) {
-        int n=grid.length;
+        int n=grid[0].length;
+        int m=grid.length;
+        int row=m-1;
+        int col=0;
         int ans=0;
-        for(int i=0;i<n;i++) {
-           int negCount=getNegativeCount(grid[i]);
-           System.out.println(negCount);
-           ans += grid[i].length-negCount;
+        while(row>=0 && col<n) {
+            if(grid[row][col]>=0) {
+                col++;
+            }
+           else {
+                ans += (n-col);
+                row--;
+            }
         }
         return ans;
     }
