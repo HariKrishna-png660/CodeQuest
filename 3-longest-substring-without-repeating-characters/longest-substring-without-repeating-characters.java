6f958c6fe7a4ccfa2d58596351511d8691b1926a
HariKrishna-png660 <htalagapu@gmail.com>
2025-10-19 14:56:20 +0530

    Time: 2 ms (98.74%) | Memory: 43.4 MB (94.01%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -6,17 +6,39 @@ class Solution {
     // ei will try to expand the window to maximize the length of the substring
     // ei may also damge the window by adding duplicates 
     
-    // si will correct the window by moving any steps forward so that every character occurs once in the window
+    // si will repair the window by moving any steps forward so that every character occurs once in the window
     public int lengthOfLongestSubstring(String s) {
-        int maxLen=0;
-        int count=0;
+        // int maxLen=0;
+        // int count=0;
+        // int n=s.length();
+        // int si=0;
+        // int ei=0;
+        // int freq[]=new int[128];
+        // while(ei<n) {
+        //     if(freq[s.charAt(ei)]==1) {
+        //        count++;
+        //     }
+        //     freq[s.charAt(ei)]++;
+        //     ei++;
+        //     while(count>0) {
+        //         if(freq[s.charAt(si)]==2) {
+        //             count--;
+        //         }
+        //         freq[s.charAt(si)]--;
+        //         si++;
+        //     }
+        //     maxLen=Math.max(maxLen,ei-si);
+        // }
+        // return maxLen;
         int n=s.length();
         int si=0;
         int ei=0;
+        int maxLen=0;
+        int count=0;
         int freq[]=new int[128];
         while(ei<n) {
             if(freq[s.charAt(ei)]==1) {
-               count++;
+                count++;
             }
             freq[s.charAt(ei)]++;
             ei++;
