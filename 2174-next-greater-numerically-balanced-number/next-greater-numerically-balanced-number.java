9e0c2022418c4426cfa86db9211d16dab3b188ff
HariKrishna-png660 <htalagapu@gmail.com>
2025-10-25 12:03:55 +0530

    Time: 653 ms (5.34%) | Memory: 41.6 MB (80.92%) - LeetSync

--- 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.java
+++ 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.java
@@ -1,27 +1,36 @@
 class Solution {
     public int nextBeautifulNumber(int n) {
-        int min=n+1;
-        int max=(int)(1e9);
-        for(int i=min;i<=max;i++) {
-            HashMap<Integer,Integer> map=new HashMap<>();
-            int num=i;
-           while(num!=0) {
-              int rem=num%10;
-              map.put(rem,map.getOrDefault(rem,0)+1);
-              num=num/10;
-           }
-           int f=1;
-           for(int key:map.keySet()) {
-              int val=map.get(key);
-              if(val!=key) {
-                 f=0;
-                 break;
-              }
-           }
-           if(f==1) {
-              return i;
-           }
+        List<Integer> list=new ArrayList<>();
+        generate(0,new int[10],list);
+        Collections.sort(list);
+        for(int num:list) {
+            if(num>n) {
+                return num;
+            }
         }
         return -1;
+    }
+    public void generate(long num,int count[],List<Integer> list) {
+        if(num>0 && isBalanced(count)) {
+            list.add((int)num);
+        }
+        if(num>1224444) {
+            return;
+        }
+        for(int d=1;d<=7;d++) {
+            if(count[d]<d) {
+                count[d]++;
+                generate(num*10+d,count,list);
+                count[d]--;
+            }
+        }
+    }
+    public boolean isBalanced(int count[]) {
+        for(int d=1;d<=7;d++) {
+            if(count[d]!=0 && count[d]!=d) {
+                return false;
+            }
+        }
+        return true;
     }
 }
