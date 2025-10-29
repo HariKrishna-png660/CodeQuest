06cb708fe0a27009113f2b9262c08b771c922ddf
HariKrishna-png660 <htalagapu@gmail.com>
2025-10-29 16:05:53 +0530

    Time: 5 ms (25.94%) | Memory: 42.8 MB (5.39%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,45 +1,23 @@
 class Solution {
-    static String[] keyPad={"/",".","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-    // recursion on the way down 
-    // public List<String> letterCombinations(String digits) {
-    //     if(digits.length()==0) {
-    //         List<String> baseAns=new ArrayList<>();
-    //         baseAns.add("");
-    //         return baseAns;
-    //     }
-    //     String remString=digits.substring(1);
-    //     char firstChar=digits.charAt(0);
-    //     System.out.println(firstChar);
-    //     int firstNum=firstChar-'0';
-    //     List<String> smallAns=letterCombinations(remString);
-    //     String str=keyPad[firstNum];
-    //     List<String> myAns=new ArrayList<>();
-    //     for(int i=0;i<str.length();i++) {
-    //         char ch=str.charAt(i);
-    //         for(String s:smallAns) {
-    //             myAns.add(ch+s+"");
-    //         }
-    //     }
-    //     return myAns;
-    // }
-    // recursion on the way up
-    public void letterCombinations(String digits,List<String> ans,String sub) {
-          if(digits.length()==0) {
-              ans.add(sub);
-              return;
-          }
-          String remString=digits.substring(1);
-          char firstChar=digits.charAt(0);
-          int firstNum=firstChar-'0';
-          String str=keyPad[firstNum];
-          for(int i=0;i<str.length();i++) {
-              char letter=str.charAt(i);
-              letterCombinations(remString,ans,sub+letter);
-          }
-    }
+    // create an array of strings
+    static String letters[]={"/",".","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
     public List<String> letterCombinations(String digits) {
+        if(digits.length()==0) {
+            ArrayList<String> baseAns=new ArrayList<>();
+            baseAns.add("");
+            return baseAns;
+        }
+        char firstChar=digits.charAt(0);
+        String remString=digits.substring(1);
+        List<String> smallAns=letterCombinations(remString);
         List<String> ans=new ArrayList<>();
-        letterCombinations(digits,ans,"");
+        String currString=letters[firstChar-'0'];
+        for(int i=0;i<currString.length();i++) {
+            char letter=currString.charAt(i);
+            for(String s:smallAns) {
+                ans.add(letter+s);
+            } 
+        }
         return ans;
     }
 }
