688c401cce55f537af9c8ddaf99a04b6a1bb014b
HariKrishna-png660 <htalagapu@gmail.com>
2025-11-04 16:04:29 +0530

    Time: 11 ms (89.47%) | Memory: 46.4 MB (5.11%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -20,7 +20,7 @@ class Solution {
     //            count++;
     //        }
     //        ei++;
-    //        if(ei-si>k) {
+    //        if(ei-si>k) { 
     //           if(isVowel(s.charAt(si))) {
     //              count--;
     //           }
@@ -30,34 +30,33 @@ class Solution {
     //    }
     //    return maxCount;
     // }
-    public boolean isVowel(String s,int i) {
-         char ch=s.charAt(i);
-         if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') {
+     public boolean isVowel(char ch) {
+        if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u') {
             return true;
-         }
-         return false;
-    }
-     public int maxVowels(String s, int k) {
-        int n=s.length();
-        int si=0;
-        int ei=0;
-        int maxVowels=0;
-        int currVowels=0;
-        while(ei<n) {
-            if(isVowel(s,ei)) {
-                currVowels++;
-            }
-            ei++;
-            if(ei-si>k) {
-                if(isVowel(s,si)) {
-                    currVowels--;
-                }
-                si++;
-            }
-            if(ei-si==k) {
-                maxVowels=Math.max(maxVowels,currVowels);
-            }
         }
-        return maxVowels;
+        return false;
+     }
+     public int maxVowels(String s, int k) {
+         int n=s.length();
+         int si=0;
+         int ei=0;
+         int count=0;
+         int maxCount=0;
+         while(ei<n) {
+             char ech=s.charAt(ei);
+             if(isVowel(ech)) {
+                   count++;
+             }
+             ei++;
+             if(ei-si>k) {
+                 char sch=s.charAt(si);
+                 if(isVowel(sch)) {
+                    count--;
+                 }
+                 si++;
+             }
+            maxCount=Math.max(maxCount,count);
+         }
+         return maxCount;
      }
 }
