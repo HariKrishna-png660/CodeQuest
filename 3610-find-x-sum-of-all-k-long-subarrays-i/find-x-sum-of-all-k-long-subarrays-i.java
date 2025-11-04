291d8088ee80cc559456164cee4c905aa5acc9b0
HariKrishna-png660 <htalagapu@gmail.com>
2025-11-04 16:46:24 +0530

    Time: 10 ms (75.17%) | Memory: 47.1 MB (7.59%) - LeetSync

--- 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.java
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.java
@@ -1,42 +1,48 @@
 class Solution {
-    public int getXsum(int arr[],int start,int end,int maxLim) {
-        int freq[]=new int[51];
-        for(int i=start;i<=end;i++) {
-            int val=arr[i];
-            freq[val]++;
-        }
-        ArrayList<ArrayList<Integer>> list=new ArrayList<>();
-        for(int i=1;i<=50;i++) {
-            int val=freq[i];
-            ArrayList<Integer> l1=new ArrayList<>();
-            l1.add(i);
-            l1.add(val);
-            list.add(l1);
-        }
-        Collections.sort(list,(a,b)-> {
-            int cmp=Integer.compare(b.get(1),a.get(1));
-            if(cmp==0) {
-                return Integer.compare(b.get(0),a.get(0));
-            }
-            return cmp;
-        });
-        int size=list.size();
-        int ans=0;
-        int i=0;
-        while(size>0 && maxLim>0) {
-            ans += (list.get(i).get(0)*list.get(i).get(1));
-            maxLim--;
-            size--;
-            i++;
-        }
-       return ans;
-    }
     public int[] findXSum(int[] nums, int k, int x) {
         int n=nums.length;
         int ans[]=new int[n-k+1];
-        for(int i=0;i<n-k+1;i++) {
-           int xSum=getXsum(nums,i,i+k-1,x);
-           ans[i]=xSum;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int si=0;
+        int ei=0;
+        int j=0;
+        while(ei<n) {
+            int i=0;
+            int val=nums[ei];
+            map.put(val,map.getOrDefault(val,0)+1);
+            ei++;
+            if(ei-si>k) {
+                int key=nums[si];
+                int value=map.get(key);
+                if(value==1) {
+                    map.remove(key);
+                }
+                else {
+                    map.put(key,value-1);
+                }
+                si++;
+            }
+            if(ei-si==k) {
+                // create a priority queue of type array 
+                PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->{
+                    if(a[1]!=b[1]) {
+                        return b[1]-a[1];
+                    }
+                    return b[0]-a[0];
+                });
+                for(int key:map.keySet()) {
+                    pq.add(new int[]{key,map.get(key)});
+                }
+                int count=0;
+                int pick=x;
+                while(pick>0 && pq.size()!=0) {
+                    int top[]=pq.remove();
+                    count += (top[0]*top[1]);
+                    pick--;
+                }
+                ans[j]=count;
+                j++;
+            }
         }
         return ans;
     }
