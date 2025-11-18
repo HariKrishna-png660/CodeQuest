62324086a8266ab7e2f820f05f9c5e35a0133d56
HariKrishna-png660 <htalagapu@gmail.com>
2025-11-18 14:46:12 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (10.22%) - LeetSync

--- 717-1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.java
+++ 717-1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.java
@@ -1,12 +1,8 @@
 class Solution {
     public boolean isOneBitCharacter(int[] bits) {
          int n=bits.length;
-         int index=-1;
          int i=0;
-         if(n==1) {
-            return true;
-         }
-         while(i<n) {
+         while(i<n-1) {
             int currVal=bits[i];
             if(currVal==0) {
                 i++;
@@ -14,10 +10,7 @@ class Solution {
             else {
                 i += 2;
             }
-            if(i==n-1) {
-                index=1;
-            }
          }
-         return index==-1 ? false:true;
+         return i==n-1;
     }
 }
