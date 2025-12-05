1a86132859f15a0f2c5eb00f57383276e1080036
HariKrishna-png660 <htalagapu@gmail.com>
2025-12-05 16:57:46 +0530

    Time: 24 ms (60.50%) | Memory: 65.4 MB (27.88%) - LeetSync

--- 525-contiguous-array/contiguous-array.java
+++ 525-contiguous-array/contiguous-array.java
@@ -1,25 +1,24 @@
 class Solution {
     public int findMaxLength(int[] nums) {
-       int n=nums.length;
-       int maxLen=0;
-       int currSum=0;
-       HashMap<Integer,Integer> map=new HashMap<>();
-       map.put(currSum,-1);
-       for(int i=0;i<n;i++) {
-           int val=nums[i];
-           if(val==0) {
-            currSum+=-1;
-           }
-           else {
-            currSum+=1;
-           }
-           if(map.containsKey(currSum)) {
-              maxLen=Math.max(maxLen,i-map.get(currSum));
-           }
-           else {
-              map.put(currSum,i);
-           }
-       }
-       return maxLen;
+        int n=nums.length;
+        int currSum=0;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int maxLength=0;
+        map.put(0,-1);
+        for(int i=0;i<n;i++) {
+            if(nums[i]==0) {
+                currSum += -1;
+            }
+            else {
+                currSum += 1;
+            }
+            if(map.containsKey(currSum)) {
+                maxLength=Math.max(maxLength,i-map.get(currSum));
+            }
+            else {
+                map.put(currSum,i);
+            }
+        }
+        return maxLength;
     }
 }
