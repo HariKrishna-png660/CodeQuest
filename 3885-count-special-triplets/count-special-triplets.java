b6944e724c398cfa4734807b09ca5c07815170f9
HariKrishna-png660 <htalagapu@gmail.com>
2025-12-09 19:34:57 +0530

    Time: 172 ms (82.17%) | Memory: 185.9 MB (47.29%) - LeetSync

--- 3885-count-special-triplets/count-special-triplets.java
+++ 3885-count-special-triplets/count-special-triplets.java
@@ -1,29 +1,17 @@
 class Solution {
     public int specialTriplets(int[] nums) {
-        int n=nums.length;
-        long Mod = 1000000007;
-        HashMap<Integer,Integer> rightMap=new HashMap<>();
-        for(int i=0;i<n;i++) {
-            int val=nums[i];
-            rightMap.put(val,rightMap.getOrDefault(val,0)+1);
+        HashMap<Integer,Integer> validI=new HashMap<>();
+        HashMap<Integer,Integer> validJ=new HashMap<>();
+        int M = (int)1e9 + 7;
+        int result=0;
+        for(int num:nums) {
+            if(num%2==0) {
+                result =(result+validJ.getOrDefault(num/2,0)) % M;
+            }
+            int updatedJ=(validJ.getOrDefault(num,0)+validI.getOrDefault(num*2,0))%M;
+            validJ.put(num,updatedJ);
+            validI.put(num,validI.getOrDefault(num,0)+1);
         }
-       HashMap<Integer,Integer> leftMap=new HashMap<>();
-       long ans=0;
-       for(int i=0;i<n;i++) {
-           int val=nums[i];
-           if(rightMap.get(val)==1) {
-                rightMap.remove(val);
-           }
-           else {
-              rightMap.put(val,rightMap.getOrDefault(val,0)-1);
-           }
-           if(leftMap.containsKey(2*val) && rightMap.containsKey(2*val)) {
-                 long left = leftMap.get(2*val);
-                 long right = rightMap.get(2*val);
-                 ans = (ans + (left*right)%Mod)%Mod;
-           }
-           leftMap.put(val,leftMap.getOrDefault(val,0)+1);
-       }
-       return (int)ans;
+        return result;
     }
 }
