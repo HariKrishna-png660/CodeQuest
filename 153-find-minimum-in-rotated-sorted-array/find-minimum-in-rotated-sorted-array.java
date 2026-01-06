c0219dc10b87d97bae6a61e5cc3d435659450246
HariKrishna-png660 <htalagapu@gmail.com>
2026-01-06 07:08:49 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (35.72%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -6,45 +6,19 @@ class Solution {
     // we only go to right when it is unsorted and remaing cases we go to left
     // going right means si=mid+1
     // going left means ei=mid
-    // public int findMin(int[] nums) {
-    //     int si=0;
-    //     int ei=nums.length-1;
-    //     while(si<ei) { // tc O(log n)
-    //         int mid=(si+ei)/2;
-    //         if(nums[mid]>nums[ei]) {
-    //             si=mid+1;
-    //         }
-    //         else {
-    //             ei=mid;
-    //         }
-    //     }
-    //     return nums[si];
-    // }
-     public int findMin(int[] nums) {
-            int si = 0, ei = nums.length - 1;
-            int ans = nums[0];
-
-             while (si <= ei) {
-                  int mid = si + (ei - si) / 2;
-
-        // If the current window is already sorted,
-        // then nums[si] is the minimum in this range.
-        if (nums[si] <= nums[ei]) {
-            ans = Math.min(ans, nums[si]);
-            break;
+    public int findMin(int[] nums) {
+        int si=0;
+        int ei=nums.length-1;
+        while(si<ei) { // tc O(log n)
+            int mid=(si+ei)/2;
+            if(nums[mid]>nums[ei]) {
+                si=mid+1;
+            }
+            else {
+                ei=mid;
+            }
+            // System.out.print(si+" "+ei);
         }
-
-        // Left half is sorted
-        if (nums[si] <= nums[mid]) {
-            ans = Math.min(ans, nums[si]);
-            si = mid + 1;
-        }
-        // Right half is sorted
-        else {
-            ans = Math.min(ans, nums[mid]);
-            ei = mid - 1;
-        }
+        return nums[si];
     }
-    return ans;
-}
-}
+   }
