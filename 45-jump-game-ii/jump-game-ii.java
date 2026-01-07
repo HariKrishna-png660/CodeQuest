e0980532453a162d7f790b312ebdc14fb06cba8a
HariKrishna-png660 <htalagapu@gmail.com>
2026-01-07 16:22:37 +0530

    Time: 38 ms (16.43%) | Memory: 47.5 MB (21.21%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,26 +1,22 @@
 class Solution {
-    public int jump_rec(int nums[],int index,int dp[]) {
-        if(index==nums.length-1) {
-            return dp[nums.length-1]=0;
-        }
-        if(dp[index]!=-1) {
-            return dp[index];
-        }
-        int minJumps=Integer.MAX_VALUE;
-        for(int i=1;i<=nums[index];i++) {
-            if(i+index<nums.length) {
-                int nextJumps=jump_rec(nums,i+index,dp);
-                if(nextJumps!=Integer.MAX_VALUE) {
-                  minJumps=Math.min(minJumps,nextJumps+1);
-                }
-            }
-        }
-        return dp[index]=minJumps;
-    }
     public int jump(int[] nums) {
         int n=nums.length;
         int dp[]=new int[n];
-        Arrays.fill(dp,-1);
-        return jump_rec(nums,0,dp);
+        dp[n-1]=0;
+        for(int i=n-2;i>=0;i--) {
+            int min=Integer.MAX_VALUE;
+            for(int j=0;j<nums[i];j++) {
+                if(i+j+1<n) {
+                   min=Math.min(min,dp[i+j+1]);
+                }
+            }
+            if(min!=Integer.MAX_VALUE) {
+                dp[i]=min+1;
+            }
+            else {
+                dp[i]=min;
+            }
+        }
+        return dp[0];
     }
 }
