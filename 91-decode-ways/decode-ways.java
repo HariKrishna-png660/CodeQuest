cced838242a370fb61e0d86d588d8ddba6d0444b
HariKrishna-png660 <htalagapu@gmail.com>
2026-01-07 12:44:48 +0530

    Time: 1 ms (85.32%) | Memory: 43.5 MB (16.24%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,89 +1,21 @@
 class Solution {
-    // recursive solution will get tle ......
-    // static int numberOfWays;
-    // public void numDecodings(String str,String psf) {
-    //     if(str.length()==0) {
-    //         numberOfWays++;
-    //         return;
-    //     }
-    //     // leading zero case
-    //     if(str.charAt(0)=='0') {
-    //         return;
-    //     }
-    //     int digit=str.charAt(0)-'0';
-    //     char cChar=(char)('a'+(digit-1));
-
-    //     String remString=str.substring(1);
-    //     numDecodings(remString,psf+cChar);
-
-    //     if(str.length()>=2) {
-    //         String first2Characters=str.substring(0,2);
-
-    //         int firstTwoDigits=Integer.parseInt(first2Characters);
-    //         if(firstTwoDigits<=26) {
-    //               cChar=(char)('a'+(firstTwoDigits-1));
-    //               remString=str.substring(2);
-    //               numDecodings(remString,psf+cChar);
-    //         }
-    //     }
-    // }
-    // memoization solution .......
-    // public int numDecodings(int idx,String str,int dp[]) {
-    //     if(idx==str.length()) {
-    //         return dp[idx]=1;
-    //     }
-    //     if(str.charAt(idx)=='0') {
-    //         return dp[idx]=0;
-    //     }
-    //     if(dp[idx]!=-1) {
-    //         return dp[idx];
-    //     }
-    //     int singleCharWays=numDecodings(idx+1,str,dp);
-    //     int twoCharWays=0;
-    //     if(idx+1<str.length()) {
-    //         int numUsing2Chars=10*(str.charAt(idx)-'0')+(str.charAt(idx+1)-'0');
-
-    //         if(numUsing2Chars <=26) {
-    //             twoCharWays=numDecodings(idx+2,str,dp);
-    //         }
-    //     }
-    //     return dp[idx]=singleCharWays+twoCharWays;
-    // }
-    // tabulation solution .....
-    public int numDecodings(int n,String str,int dp[]) {
-        for(int idx=n;idx>=0;idx--) {
-            if(idx==n) {
-                dp[idx]=1;
-                continue;
-            }
-            if(str.charAt(idx)=='0') {
-                dp[idx]=0;
-                continue;
-            }
-            int singleCharWays=dp[idx+1];
-            int twoCharWays=0;
-            
-            if(idx+1<n) {
-                int numUsing2Chars=10*(str.charAt(idx)-'0') +(str.charAt(idx+1)-'0');
-                if(numUsing2Chars<=26) {
-                    twoCharWays=dp[idx+2];
-                }
-            }
-            dp[idx]=singleCharWays+twoCharWays;
-        }
-        return dp[0];
-    }
     public int numDecodings(String s) {
-        // numberOfWays=0;
-        // numDecodings(s,"");
-        // return numberOfWays;
-        // =================
-        // int n=s.length();
-        // int dp[]=new int[n+1];
-        // Arrays.fill(dp,-1);
-        // return numDecodings(0,s,dp);
         int n=s.length();
         int dp[]=new int[n+1];
-        return numDecodings(n,s,dp);
+        dp[n]=1;
+        for(int i=n-1;i>=0;i--) {
+            if(s.charAt(i)=='0') {
+                continue;
+            }
+            dp[i]=dp[i+1];
+            if(i+2<=n) {
+                  String sub=s.substring(i,i+2);
+                  int val=Integer.parseInt(sub);
+                   if(val<=26) {
+                      dp[i] += dp[i+2];
+                    }
+            }
+        }
+        return dp[0];
     }
 }
