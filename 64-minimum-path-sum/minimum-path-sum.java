5bae73dd3735dfeff09df8f7d61ba767e4ab5f89
HariKrishna-png660 <htalagapu@gmail.com>
2026-01-07 11:11:37 +0530

    Time: 7 ms (10.61%) | Memory: 50.4 MB (37.13%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,69 +1,25 @@
 class Solution {
-    // // memoization 
-    // public int minPathSum(int sr,int sc,int dr,int dc,int grid[][],int dp[][]) {
-    //     if(sr==dr && sc==dc) {
-    //         return dp[dr][dc]=grid[dr][dc];
-    //     }
-    //     if(dp[sr][sc]!=-1) {
-    //         return dp[sr][sc];
-    //     }
-    //     int minSum=Integer.MAX_VALUE;
-    //     if(sr+1<=dr) {
-    //         minSum=minPathSum(sr+1,sc,dr,dc,grid,dp);
-    //     }
-    //     if(sc+1<=dc) {
-    //         minSum=Math.min(minSum,minPathSum(sr,sc+1,dr,dc,grid,dp));
-    //     }
-    //     return dp[sr][sc]=minSum+grid[sr][sc];
-    // }
-    // tabulation solution ....
-    public int minPathSum(int dr,int dc,int grid[][],int dp[][]) {
-        //  for(int j=dc;j>=0;j--) {
-        // for(int i=dr;i>=0;i--) {
-        //         if(i==dr && j==dc) {
-        //             tab[dr][dc]=grid[dr][dc];
-        //             continue;
-        //         }
-        //         int minSum=Integer.MAX_VALUE;
-        //         if(i+1<=dr) {
-        //             minSum=Math.min(minSum,tab[i+1][j]);
-        //         }
-        //         if(j+1<=dc) {
-        //            minSum=Math.min(minSum,tab[i][j+1]);
-        //         }
-        //         minSum += grid[i][j];
-        //         tab[i][j]=minSum;
-        //     }
-        // }
-        // return tab[0][0];
-        for(int i=dr;i>=0;i--) {
-            for(int j=dc;j>=0;j--) {
-               if(i==dr && j==dc) {
-                   dp[dr][dc]=grid[dr][dc];
-                   continue;
-               }
-               int minSum=Integer.MAX_VALUE;
-               if(i+1<=dr) {
-                  minSum=Math.min(minSum,dp[i+1][j]);
-               }
-               if(j+1<=dc) {
-                  minSum=Math.min(minSum,dp[i][j+1]);
-               }
-               minSum += grid[i][j];
-               dp[i][j]=minSum;
-            }
-        }
-
-        return dp[0][0];
-    }
     public int minPathSum(int[][] grid) {
         int m=grid.length;
         int n=grid[0].length;
         int dp[][]=new int[m][n];
-        // for(int i=0;i<m;i++) {
-        //     Arrays.fill(dp[i],-1);
-        // }
-        // return minPathSum(0,0,m-1,n-1,grid,dp);
-        return minPathSum(m-1,n-1,grid,dp);
+        for(int j=n-1;j>=0;j--) {
+            for(int i=m-1;i>=0;i--) {
+                if(i==m-1 && j==n-1) {
+                    dp[i][j]=grid[i][j];
+                    continue;
+                }
+                dp[i][j]=grid[i][j];
+                int min=Integer.MAX_VALUE;
+                if(i+1<m) {
+                   min=Math.min(min,dp[i+1][j]);
+                }
+                if(j+1<n) {
+                   min=Math.min(min,dp[i][j+1]); 
+                }
+                dp[i][j] += min;
+            }
+        }
+        return dp[0][0];
     }
 }
