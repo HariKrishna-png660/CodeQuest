1cfb983ac6f805a8157ef1f1820a7d90948a3316
HariKrishna-png660 <htalagapu@gmail.com>
2026-01-08 07:05:39 +0530

    Time: 17 ms (81.59%) | Memory: 54.3 MB (66.55%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,30 +1,23 @@
 class Solution {
-    public int numDistinct(int n,int m,String s,String t,int dp[][]) {
-        if(m==0) {
-            return 1;
-        }
-        if(n<m) {
-            return 0;
-        }
-        if(dp[n][m]!=-1) {
-            return dp[n][m];
-        }
-        int count=0;
-        if(s.charAt(n-1)==t.charAt(m-1)) {
-           count = numDistinct(n-1,m-1,s,t,dp)+numDistinct(n-1,m,s,t,dp);
-        }
-        else {
-            count = numDistinct(n-1,m,s,t,dp);
-        }
-        return dp[n][m]=count;
-    }
     public int numDistinct(String s, String t) {
         int n=s.length();
         int m=t.length();
         int dp[][]=new int[n+1][m+1];
-        for(int d[]:dp) {
-            Arrays.fill(d,-1);
+        for(int j=0;j<=m;j++) {
+            for(int i=0;i<=n;i++) {
+                if(i<j) {
+                    continue;
+                }
+                if(j==0) {
+                    dp[i][j]=1;
+                    continue;
+                }
+                dp[i][j]=dp[i-1][j];
+                if(s.charAt(i-1)==t.charAt(j-1)) {
+                    dp[i][j] += dp[i-1][j-1];
+                }
+            }
         }
-        return numDistinct(n,m,s,t,dp);
+        return dp[n][m];
     }
 }
