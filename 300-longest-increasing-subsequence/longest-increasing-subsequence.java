730cb33e1fd7fd229e5234095fa65b66d3e0f180
HariKrishna-png660 <htalagapu@gmail.com>
2026-01-08 17:31:39 +0530

    Time: 79 ms (25.99%) | Memory: 46.2 MB (57.62%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,35 +1,23 @@
 class Solution {
-    // time complexity is nlogn
-    public int findInsertionIndex(ArrayList<Integer> lis,int ele) {
-        int start=0;
-        int end=lis.size()-1;
-         while(start<=end) {
-            int mid=(start+end)/2;
-            if(lis.get(mid)==ele) {
-                return mid;
-            }
-            else if(lis.get(mid)<ele) {
-                start=mid+1;
-            }
-            else {
-                end=mid-1;
-            }
+    public int lengthOfLIS(int idx,int dp[],int nums[]) {
+         if(dp[idx]!=0) {
+            return dp[idx];
          }
-         return start;
+         int ans=0;
+         for(int j=idx-1;j>=0;j--) {
+               if(nums[j]<nums[idx]) {
+                  ans=Math.max(ans,lengthOfLIS(j,dp,nums));
+               }
+         }
+         return dp[idx]=ans+1;
     }
     public int lengthOfLIS(int[] nums) {
         int n=nums.length;
-        ArrayList<Integer> lis=new ArrayList<>();
+        int dp[]=new int[n];
+        int res=0;
         for(int i=0;i<n;i++) {
-            int ele=nums[i];
-            int idx=findInsertionIndex(lis,ele);
-            if(idx==lis.size()) {
-                lis.add(ele);
-            }
-            else {
-                lis.set(idx,ele);
-            }
+            res=Math.max(res,lengthOfLIS(i,dp,nums));
         }
-        return lis.size();
+        return res;
     }
 }
