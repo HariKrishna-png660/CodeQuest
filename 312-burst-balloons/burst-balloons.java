e5173162f80c455c25f301ab8e3a4ab64af94953
HariKrishna-png660 <htalagapu@gmail.com>
2026-01-09 21:35:01 +0530

    Time: 134 ms (6.90%) | Memory: 44.3 MB (67.44%) - LeetSync

--- 312-burst-balloons/burst-balloons.java
+++ 312-burst-balloons/burst-balloons.java
@@ -1,18 +1,15 @@
 class Solution {
-    public int maxCoins_memo(int nums[],int si,int ei,int n,int dp[][]) {
+    public int maxCoins(int si,int ei,int nums[],int dp[][],int n) {
         if(dp[si][ei]!=-1) {
             return dp[si][ei];
         }
-        int leftVal=si==0 ? 1:nums[si-1];
+        int leftVal=si==0?1:nums[si-1];
         int rightVal=ei==n-1?1:nums[ei+1];
-
         int res=0;
         for(int cut=si;cut<=ei;cut++) {
-            int left=cut==si?0:maxCoins_memo(nums,si,cut-1,n,dp);
-            int right=cut==ei?0:maxCoins_memo(nums,cut+1,ei,n,dp);
-
+            int left=cut==si?0:maxCoins(si,cut-1,nums,dp,n);
+            int right=cut==ei?0:maxCoins(cut+1,ei,nums,dp,n);
             int curr_ans=left+right+leftVal*nums[cut]*rightVal;
-
             res=Math.max(res,curr_ans);
         }
         return dp[si][ei]=res;
@@ -23,6 +20,6 @@ class Solution {
         for(int d[]:dp) {
             Arrays.fill(d,-1);
         }
-        return maxCoins_memo(nums,0,n-1,n,dp);
+        return maxCoins(0,n-1,nums,dp,n);
     }
 }
