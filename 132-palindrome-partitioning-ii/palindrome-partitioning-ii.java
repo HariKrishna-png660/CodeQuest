9a7b76fa3bb253acaeb7ff4ee954c334c0109e02
HariKrishna-png660 <htalagapu@gmail.com>
2026-01-10 00:48:25 +0530

    Time: 45 ms (50.42%) | Memory: 47.6 MB (36.11%) - LeetSync

---
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
@@ -0,0 +1,47 @@
+class Solution {
+    public boolean[][] makeIsPalindrome(String s) {
+        int n=s.length();
+        
+        boolean dp[][]=new boolean[n][n];
+
+        for(int gap=0;gap<n;gap++) {
+            for(int i=0,j=gap;j<n;i++,j++) {
+                if(i==j) {
+                    dp[i][j]=true;
+                    continue;
+                }
+                else if(i+1==j) {
+                    if(s.charAt(i)==s.charAt(j)) {
+                        dp[i][j]=true;
+                    }
+                }
+                else {
+                    if(s.charAt(i)==s.charAt(j)) {
+                        dp[i][j]=dp[i+1][j-1];
+                    }
+                }
+            }
+        }
+        return dp;
+    }
+    public int minCut_rec(String s,int idx,boolean isPalindrome[][],int dp[]) {
+         if(isPalindrome[idx][s.length()-1]==true) {
+               return dp[idx]=0;
+         }
+         if(dp[idx]!=0) {
+            return dp[idx];
+         }
+         int res=(int)(1e8);
+         for(int cut=idx;cut<s.length();cut++) {
+            if(isPalindrome[idx][cut]) {
+                res=Math.min(res,minCut_rec(s,cut+1,isPalindrome,dp)+1);
+            }
+         }
+         return dp[idx]=res;
+    }
+    public int minCut(String s) {
+        boolean isPalindrome[][]=makeIsPalindrome(s);
+        int dp[]=new int[s.length()];
+        return minCut_rec(s,0,isPalindrome,dp);
+    }
+}
