d8ce1fb90a06d4478ff602a9157810405770a7cf
HariKrishna-png660 <htalagapu@gmail.com>
2026-01-09 21:50:45 +0530

    Time: 33 ms (98.42%) | Memory: 44.3 MB (82.39%) - LeetSync

--- 312-burst-balloons/burst-balloons.java
+++ 312-burst-balloons/burst-balloons.java
@@ -1,25 +1,22 @@
 class Solution {
-    public int maxCoins(int si,int ei,int nums[],int dp[][],int n) {
-        if(dp[si][ei]!=-1) {
-            return dp[si][ei];
-        }
-        int leftVal=si==0?1:nums[si-1];
-        int rightVal=ei==n-1?1:nums[ei+1];
-        int res=0;
-        for(int cut=si;cut<=ei;cut++) {
-            int left=cut==si?0:maxCoins(si,cut-1,nums,dp,n);
-            int right=cut==ei?0:maxCoins(cut+1,ei,nums,dp,n);
-            int curr_ans=left+right+leftVal*nums[cut]*rightVal;
-            res=Math.max(res,curr_ans);
-        }
-        return dp[si][ei]=res;
-    }
     public int maxCoins(int[] nums) {
         int n=nums.length;
         int dp[][]=new int[n][n];
-        for(int d[]:dp) {
-            Arrays.fill(d,-1);
+        for(int gap=0;gap<n;gap++) {
+            for(int si=0,ei=gap;ei<n;si++,ei++) {
+                int leftVal=si==0?1:nums[si-1];
+                int rightVal=ei==n-1?1:nums[ei+1];
+
+                int res=0;
+                for(int cut=si;cut<=ei;cut++) {
+                    int left=cut==si?0:dp[si][cut-1];
+                    int right=cut==ei?0:dp[cut+1][ei];
+                    int curr_ans=left+right+leftVal*rightVal*nums[cut];
+                    res=Math.max(res,curr_ans);
+                }
+                dp[si][ei]=res;
+            }
         }
-        return maxCoins(0,n-1,nums,dp,n);
+        return dp[0][n-1];
     }
 }
