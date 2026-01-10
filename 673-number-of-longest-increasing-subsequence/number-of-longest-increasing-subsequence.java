cb3bc44d2a79b9600fa08fc104272ca02aba1619
HariKrishna-png660 <htalagapu@gmail.com>
2026-01-10 16:39:52 +0530

    Time: 18 ms (94.68%) | Memory: 45.5 MB (45.67%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.java
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.java
@@ -1,37 +1,33 @@
 class Solution {
     public int findNumberOfLIS(int[] nums) {
         int n=nums.length;
-        int dp[]=new int[n];
         int count[]=new int[n];
-        Arrays.fill(dp,1);
+        int dp[]=new int[n];
         Arrays.fill(count,1);
+        Arrays.fill(dp,1);
         for(int i=1;i<n;i++) {
             for(int j=i-1;j>=0;j--) {
                 if(nums[j]<nums[i]) {
-                    if(dp[i]<dp[j]+1) {
+                     if(dp[i]<dp[j]+1) {
                         dp[i]=dp[j]+1;
                         count[i]=count[j];
-                    }
-                    else if(dp[i]==dp[j]+1) {
-                        count[i]+=count[j];
-                    }
+                     }
+                     else if(dp[i]==dp[j]+1) {
+                        count[i] += count[j];
+                     }
                 }
             }
         }
-        int max=0;
+        int maxLen=Integer.MIN_VALUE;
         for(int i=0;i<n;i++) {
-            max=Math.max(max,dp[i]);
+            maxLen=Math.max(maxLen,dp[i]);
         }
-        ArrayList<Integer> ans=new ArrayList<>();
+        int ans=0;
         for(int i=0;i<n;i++) {
-            if(dp[i]==max) {
-                ans.add(i);
+            if(dp[i]==maxLen) {
+                ans += count[i];
             }
         }
-        int  nlis=0;
-        for(int val:ans) {
-            nlis += count[val];
-        }
-       return nlis;
+        return ans;
     }
 }
