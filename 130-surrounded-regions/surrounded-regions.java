648d69e595da109fe08357796a7d70a317721151
HariKrishna-png660 <htalagapu@gmail.com>
2026-01-11 20:43:36 +0530

    Time: 3 ms (31.55%) | Memory: 47.6 MB (74.94%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,14 +1,15 @@
 class Solution {
-    public void solve_dfs(int row,int col,char board[][],boolean vis[][],int n,int m) {
-        board[row][col]='!';
+    public void explore(int row,int col,char board[][],boolean vis[][]) {
+        if(row<0 ||col<0 || row>=board.length || col>=board[0].length || vis[row][col] ||board[row][col]=='X') {
+            return;
+        }
         vis[row][col]=true;
+        board[row][col]='?';
         int dirs[][]={{-1,0},{0,-1},{1,0},{0,1}};
         for(int dir[]:dirs) {
-            int x=dir[0]+row;
-            int y=dir[1]+col;
-            if(x>=0 && x<n && y>=0 && y<m && vis[x][y]==false && board[x][y]=='O') {
-                solve_dfs(x,y,board,vis,n,m);
-            } 
+            int x=row+dir[0];
+            int y=col+dir[1];
+            explore(x,y,board,vis);
         }
     }
     public void solve(char[][] board) {
@@ -17,19 +18,18 @@ class Solution {
         boolean vis[][]=new boolean[n][m];
         for(int i=0;i<n;i++) {
             for(int j=0;j<m;j++) {
-                if((i==0 || i==n-1 || j==0 || j==m-1 )&& vis[i][j]==false && board[i][j]=='O') {
-                   solve_dfs(i,j,board,vis,n,m);
+                if((i==0||i==n-1||j==0||j==m-1) && board[i][j]=='O') {
+                    explore(i,j,board,vis);
                 }
             }
         }
-
         for(int i=0;i<n;i++) {
             for(int j=0;j<m;j++) {
-                if(board[i][j]=='O') {
-                    board[i][j]='X';
-                }
-                else if(board[i][j]=='!') {
+                if(board[i][j]=='?') {
                     board[i][j]='O';
+                }
+                else {
+                    board[i][j]='X';
                 }
             }
         }
