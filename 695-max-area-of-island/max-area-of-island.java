16216961a6d7d0b50ddad13f37259d08f89e1078
HariKrishna-png660 <htalagapu@gmail.com>
2026-01-11 20:17:01 +0530

    Time: 3 ms (31.05%) | Memory: 46.9 MB (13.61%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,34 +1,30 @@
 class Solution {
-    public int maxAreaIsland_dfs(int row,int col,int grid[][],boolean vis[][],int n,int m) {
-        int ans=1;
+    public int areaOfIsland(int grid[][],boolean vis[][],int row,int col) {
+        if(row<0 || row>=grid.length || col<0 || col>=grid[0].length || grid[row][col]==0 || vis[row][col]) {
+            return 0;
+        }
         vis[row][col]=true;
         int dirs[][]={{-1,0},{0,-1},{1,0},{0,1}};
+        int area=0;
         for(int dir[]:dirs) {
-            int x=dir[0]+row;
-            int y=dir[1]+col;
-            if(x>=0 && x<n && y>=0 && y<m && grid[x][y]==1 && vis[x][y]==false) {
-                 ans += maxAreaIsland_dfs(x,y,grid,vis,n,m);
-            }
+            int x=row+dir[0];
+            int y=col+dir[1];
+            area += areaOfIsland(grid,vis,x,y);
         }
-        return ans;
+        return area+1;
     }
     public int maxAreaOfIsland(int[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
-        boolean vis[][]=new boolean[n][m];
-
         int maxArea=Integer.MIN_VALUE;
+        boolean vis[][]=new boolean[n][m];
         for(int i=0;i<n;i++) {
             for(int j=0;j<m;j++) {
-                if(grid[i][j]==1 && vis[i][j]==false) {
-                    int area=maxAreaIsland_dfs(i,j,grid,vis,n,m);
-                    maxArea=Math.max(maxArea,area);
+                if(grid[i][j]==1 && !vis[i][j]) {
+                     maxArea=Math.max(maxArea,areaOfIsland(grid,vis,i,j));
                 }
             }
         }
-        if(maxArea==Integer.MIN_VALUE) {
-            return 0;
-        }
-        return maxArea;
+        return maxArea==Integer.MIN_VALUE ? 0:maxArea;
     }
 }
