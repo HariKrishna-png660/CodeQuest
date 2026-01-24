baa6ad43aa318b40ea9bce783201d270ec8f230d
HariKrishna-png660 <htalagapu@gmail.com>
2026-01-24 13:25:47 +0530

    Time: 2 ms (97.37%) | Memory: 44.7 MB (93.70%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,56 +1,27 @@
 class Solution {
-    // Sliding window 
-    // it has two pointers -> si,ei
+    public int lengthOfLongestSubstring(String str) {
+        int n=str.length();
+    int si=0;
+    int ei=0;
+    int maxLen=0;
+    int count=0;
+    int freq[]=new int[128];
+    while(ei<n) {
+      if(freq[str.charAt(ei)]==1) {
+            count++;
+      }
+      freq[str.charAt(ei)]++;
+      ei++;
+      while(count>0) {
+         if(freq[str.charAt(si)]==2) {
+             count--;
+          }
+         freq[str.charAt(si)]--;
+         si++;
+      }
+      maxLen=Math.max(maxLen,ei-si);
+    }
+    return maxLen;
 
-    // ei will increase it's value by one every time
-    // ei will try to expand the window to maximize the length of the substring
-    // ei may also damge the window by adding duplicates 
-    
-    // si will repair the window by moving any steps forward so that every character occurs once in the window
-    public int lengthOfLongestSubstring(String s) {
-        // int maxLen=0;
-        // int count=0;
-        // int n=s.length();
-        // int si=0;
-        // int ei=0;
-        // int freq[]=new int[128];
-        // while(ei<n) {
-        //     if(freq[s.charAt(ei)]==1) {
-        //        count++;
-        //     }
-        //     freq[s.charAt(ei)]++;
-        //     ei++;
-        //     while(count>0) {
-        //         if(freq[s.charAt(si)]==2) {
-        //             count--;
-        //         }
-        //         freq[s.charAt(si)]--;
-        //         si++;
-        //     }
-        //     maxLen=Math.max(maxLen,ei-si);
-        // }
-        // return maxLen;
-        int si=0;
-        int ei=0;
-        int n=s.length();
-        int freq[]=new int[128];
-        int count=0;
-        int maxLen=0;
-        while(ei<n) {
-            if(freq[s.charAt(ei)]==1) {
-                count++;
-            }
-            freq[s.charAt(ei)]++;
-             ei++;
-            while(count>0) {
-                if(freq[s.charAt(si)]==2) {
-                    count--;
-                }
-                freq[s.charAt(si)]--;
-                si++;
-            }
-            maxLen=Math.max(maxLen,ei-si);
-        }
-        return maxLen;
     }
 }
