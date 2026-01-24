e44ec8bc60360dd3aef23318db3e66bacc49ab1a
HariKrishna-png660 <htalagapu@gmail.com>
2026-01-24 21:51:03 +0530

    Time: 3 ms (87.11%) | Memory: 48.5 MB (18.02%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -1,22 +1,18 @@
 class Solution {
-    // in this question we need cannot apply binary search as we  did in the search a 2 d matrix 1
-    // because these rows continution will not form any sortedd array
-    // so we need to find such a starting position so that we can go through one of the directions when we are comparing that with the target
     public boolean searchMatrix(int[][] matrix, int target) {
-        // two such positions are 0,n-1 and m-1,0
         int m=matrix.length;
         int n=matrix[0].length;
-        int row=0;
-        int col=n-1;
+        int row=m-1;
+        int col=0;
         while(row>=0 && row<m && col>=0 && col<n) {
             if(matrix[row][col]==target) {
                 return true;
             }
-            else if(matrix[row][col]>target) {
-                col--;
+            else if(matrix[row][col]<target) {
+                 col++;
             }
             else {
-                row++;
+               row--;
             }
         }
         return false;
