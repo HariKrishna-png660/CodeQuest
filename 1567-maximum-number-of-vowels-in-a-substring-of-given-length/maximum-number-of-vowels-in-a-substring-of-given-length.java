75256c0bca60edf9eaeb7a0cd5e478c1e54fa430
HariKrishna-png660 <htalagapu@gmail.com>
2026-01-24 13:54:32 +0530

    Time: 14 ms (57.45%) | Memory: 46 MB (97.95%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,62 +1,28 @@
 class Solution {
-    // sliding window 
-    // ei will move by one step each time
-    // if the window gets damaged then si will move forward 
-    // int this process try to count max number of vowels.
-    // public boolean isVowel(char ch) {
-    //     if(ch=='a'||ch=='e' || ch=='i' ||ch=='o' ||ch=='u') {
-    //         return true;
-    //     }
-    //     return false;
-    // }
-    // public int maxVowels(String s, int k) {
-    //    int n=s.length();
-    //    int si=0;
-    //    int ei=0;
-    //    int count=0;
-    //    int maxCount=0;
-    //    while(ei<n) {
-    //        if(isVowel(s.charAt(ei))) {
-    //            count++;
-    //        }
-    //        ei++;
-    //        if(ei-si>k) { 
-    //           if(isVowel(s.charAt(si))) {
-    //              count--;
-    //           }
-    //           si++;
-    //        }
-    //       maxCount=Math.max(maxCount,count);
-    //    }
-    //    return maxCount;
-    // }
-     public boolean isVowel(char ch) {
-        if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u') {
-            return true;
+    public boolean isVowel(char ch) {
+        return ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u';
+    }
+    public int maxVowels(String s, int k) {
+        int n=s.length();
+        int vowelCount=0;
+        int si=0;
+        int ei=0;
+        int maxVowelCount=0;
+        while(ei<n) {
+            char charAtEi=s.charAt(ei);
+            if(isVowel(charAtEi)) {
+                vowelCount++;
+            }
+            ei++;
+            if(ei-si>k) {
+                char charAtSi=s.charAt(si);
+                if(isVowel(charAtSi)) {
+                    vowelCount--;
+                }
+                si++;
+            } 
+            maxVowelCount=Math.max(maxVowelCount,vowelCount);
         }
-        return false;
-     }
-     public int maxVowels(String s, int k) {
-         int n=s.length();
-         int si=0;
-         int ei=0;
-         int count=0;
-         int maxCount=0;
-         while(ei<n) {
-             char ech=s.charAt(ei);
-             if(isVowel(ech)) {
-                   count++;
-             }
-             ei++;
-             if(ei-si>k) {
-                 char sch=s.charAt(si);
-                 if(isVowel(sch)) {
-                    count--;
-                 }
-                 si++;
-             }
-            maxCount=Math.max(maxCount,count);
-         }
-         return maxCount;
-     }
+        return maxVowelCount;
+    }
 }
