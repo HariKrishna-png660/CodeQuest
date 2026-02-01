b2862d0252c9a98120c294ed442ca7889813b2a6
HariKrishna-png660 <htalagapu@gmail.com>
2026-02-01 11:07:00 +0530

    Time: 1 ms (99.80%) | Memory: 48.4 MB (74.58%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,38 +1,18 @@
 class Solution {
-    public int binarySearch(int arr[],int tar,int start,int end) {
-        int n=arr.length;
-        int low=start;
-        int high=end;
-        int ans=-1;
-        while(low<=high) {
-            int mid=(low+high)/2;
-            if(arr[mid]==tar) {
-                ans=mid;
-                low=mid+1;
+    public int[] twoSum(int[] numbers, int target) {
+        int n=numbers.length;
+        int start=0;
+        int end=n-1;
+        while(start<end) {
+            int sum=numbers[start]+numbers[end];
+            if(sum==target) {
+                return new int[]{start+1,end+1};
             }
-            else if(arr[mid]<tar) {
-                low=mid+1;
+            else if(sum<target) {
+                 start++;
             }
             else {
-                high=mid-1;
-            }
-        }
-        return ans;
-    }
-    public int[] twoSum(int[] numbers, int target) {
-        // fix the curr Element and search for the req number by binary search 
-        int n=numbers.length;
-        // iterate on each of the element in the array
-        for(int i=0;i<n;i++) {
-            int curr=numbers[i];
-            int req=target-curr;
-            int index=binarySearch(numbers,req,i,n-1);
-            if(index != -1 && index != i) {
-                int ans[]=new int[2];
-                ans[0]=i+1;
-                ans[1]=index+1;
-                Arrays.sort(ans);
-                return ans;
+                end--;
             }
         }
         return new int[]{-1,-1};
