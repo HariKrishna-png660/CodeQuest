a9c4f65aa5a7164d85c0219f8edcbb102b1f5df0
HariKrishna-png660 <htalagapu@gmail.com>
2026-02-01 15:34:05 +0530

    Time: 109 ms (80.99%) | Memory: 47.2 MB (27.03%) - LeetSync

--- 454-4sum-ii/4sum-ii.java
+++ 454-4sum-ii/4sum-ii.java
@@ -1,17 +1,17 @@
 class Solution {
     public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
         HashMap<Integer,Integer> map=new HashMap<>();
-        int count=0;
-
-        for(int e:nums1) {
-            for(int f:nums2) {
-                map.put(e+f,map.getOrDefault(e+f,0)+1);
+        // check for all the possible sums that can be formed from any  two arrays that is nums1 and nums2 or nums1 and nums3 or nums1 and nums4 
+        // after adding all the sums in hashmap then after that we need to check for these sums 
+        for(int i:nums1) {
+            for(int j:nums2) {
+                map.put(i+j,map.getOrDefault(i+j,0)+1);
             }
         }
-
-        for(int a:nums3) {
-            for(int b:nums4) {
-                count += map.getOrDefault(-(a+b),0);
+        int count=0;
+        for(int k:nums3) {
+            for(int l:nums4) {
+                count += map.getOrDefault(-(k+l),0);
             }
         }
         return count;
