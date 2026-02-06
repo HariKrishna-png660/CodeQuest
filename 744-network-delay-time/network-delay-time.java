b1e77ac5cab8a567c72a4a3c00982f1b203fcc2f
HariKrishna-png660 <htalagapu@gmail.com>
2026-02-07 01:33:51 +0530

    Time: 9 ms (95.65%) | Memory: 48.9 MB (80.46%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,57 +1,48 @@
 class Solution {
-    class Pair {
-        int vtx;
-        int wsf;
-        public Pair(int vtx,int wsf) {
-            this.vtx=vtx;
-            this.wsf=wsf;
+    public int networkDelayTime(int[][] times, int n, int k) {
+        // dis array to track the distances from the src vtx to other vertex
+        int dis[]=new int[n+1];
+        Arrays.fill(dis,(int)1e8);
+        dis[k]=0;
+        // create a graph using the edges
+        ArrayList<int[]> graph[]=new ArrayList[n+1];
+        for(int i=1;i<=n;i++) {
+            graph[i]=new ArrayList<>();
         }
-    }
-    public int[] dijkstra(ArrayList<int[]> graph[],int src,int n) {
-        int dis[]=new int[n];
-        Arrays.fill(dis,(int)(1e8));
-        PriorityQueue<Pair> pq=new PriorityQueue<>((Pair a,Pair b)->{
-              return a.wsf-b.wsf;
+        for(int edge[]:times) {
+            int u=edge[0];
+            int v=edge[1];
+            int w=edge[2];
+            graph[u].add(new int[]{v,w});
+            // graph[v].add(new int[]{u,w});
+        }
+        PriorityQueue<int[]> pq=new PriorityQueue<>((int a[],int b[])-> {
+                 return a[1]-b[1];
         });
-        dis[src]=0;
-        pq.add(new Pair(src,0));
-        while(pq.size()>0) {
-            Pair top=pq.remove();
-            int vtx=top.vtx;
-            int wsf=top.wsf;
+        pq.add(new int[]{k,0});
+        while(!pq.isEmpty()) {
+            int top[]=pq.remove();
+            int vtx=top[0];
+            int wsf=top[1];
             if(dis[vtx]<wsf) {
                 continue;
-            } 
+            }
             for(int edge[]:graph[vtx]) {
                 int nbr=edge[0];
                 int wt=edge[1];
                 if(dis[nbr]>wsf+wt) {
                     dis[nbr]=wsf+wt;
-                    pq.add(new Pair(nbr,wsf+wt));
+                    pq.add(new int[]{nbr,wsf+wt});
                 }
             }
         }
-        return dis;
-    }
-    public int networkDelayTime(int[][] times, int n, int k) {
-        ArrayList<int[]> graph[]=new ArrayList[n];
-        for(int i=0;i<n;i++) {
-            graph[i]=new ArrayList<>();
-        }
-        for(int edge[]:times) {
-            int u=edge[0]-1;
-            int v=edge[1]-1;
-            int w=edge[2];
-            graph[u].add(new int[]{v,w});
-        }
-        int dis[]=dijkstra(graph,k-1,n);
-        int maxDis=Integer.MIN_VALUE;
-        for(int i=0;i<n;i++) {
-            if(dis[i]==(int)(1e8)) {
-                return -1;
+        int maxTime=Integer.MIN_VALUE;
+        for(int i=1;i<=n;i++) {
+            if(i==k) {
+                continue;
             }
-            maxDis=Math.max(maxDis,dis[i]);
+            maxTime=Math.max(maxTime,dis[i]);
         }
-        return maxDis;
+        return maxTime==(1e8)?-1:maxTime;
     }
 }
