692bd6fea7be7b5faef8dc844e6ee533b743f768
HariKrishna-png660 <htalagapu@gmail.com>
2026-02-07 17:51:27 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (11.79%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.java
@@ -1,62 +1,27 @@
 class Solution {
-    // example
-    //  0 1 2 3 4
-    // given array:[2,3,4,7,11]
-    // original   :[1,2,3,4,5]
-    // diff       :[1,1,1,3,6]
-    // should check the maximum value of diff less than k.
-    // ans is that index+1+k
-    //     public int findKthPositive(int[] arr, int k) {
-    //     int ans=-1;
-    //     for(int i=0;i<arr.length;i++) {   // time complexity is O(n)
-    //         int expectedNum=i+1;
-    //         int diff=arr[i]-expectedNum;
-    //         if(diff<k) {
-    //             ans=i;
-    //         }
-    //         else {
-    //             break;
-    //         }
-    //     }
-    //     return ans+k+1;
-    // }
-    // using binary search 
-    // public int findKthPositive(int arr[],int k) {
-    //       int n=arr.length;
-    //       int si=0;
-    //       int ei=n-1;
-    //       int ans=-1;
-    //       while(si<=ei) {  // time complexity is O(log n)
-    //          int mid=(si+ei)/2;
-    //          int expectedNum=mid+1;
-    //          int diff=arr[mid]-expectedNum;
-    //          if(diff<k) {
-    //             ans=mid;
-    //             si=mid+1;
-    //          }
-    //          else {
-    //             ei=mid-1;
-    //          }
-    //       }
-    //       return ans+1+k;
-    // }
-      public int findKthPositive(int arr[],int k) {
-            int n=arr.length;
-            int si=0;
-            int ei=n-1;
-            int ans=-1;
-            while(si<=ei) {
-                int mid=(si+ei)/2;
-                int expectedNum=(mid+1);
-                int diff=arr[mid]-expectedNum;
-                if(diff<k) {
-                    ans=mid;
-                    si=mid+1;
-                }
-                else {
-                    ei=mid-1;
-                }
+    public int findInsertionIndex(int arr[],int tar) {
+        int n=arr.length;
+        int low=0;
+        int high=n-1;
+        int ans=-1;
+        while(low<=high) {
+            int mid=(low+high)/2;
+            if(arr[mid]<tar) {
+                 low=mid+1;
+                 ans=mid;
             }
-            return ans+1+k;
-      }
+            else {
+                 high=mid-1;
+            }
+        }
+        return ans;
+    }
+    public int findKthPositive(int[] arr, int k) {
+        int n=arr.length;
+        int diff[]=new int[n];
+        for(int i=1;i<=n;i++) {
+            diff[i-1]=arr[i-1]-i;
+        }
+        return findInsertionIndex(diff,k)+k+1;
+    }
 }
