407a2043b708a91e077a5bc8f628943939dda327
HariKrishna-png660 <htalagapu@gmail.com>
2026-02-07 15:37:27 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (98.81%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,94 +1,48 @@
 class Solution {
-    // // while finding the first index we will move towards left
-    // // while finding the last index we will move towards right
-    // public int getFirstIndex(int arr[],int target) {
-    //     int n=arr.length;
-    //     int si=0;
-    //     int ei=n-1;
-    //     int firstIndex=-1;
-    //     while(si<=ei) {
-    //         int mid=(si+ei)/2;
-    //         if(arr[mid]==target) {
-    //             firstIndex=mid;
-    //             ei=mid-1; // search for some better smaller index
-    //         }
-    //         else if(arr[mid]>target) {
-    //             ei=mid-1;
-    //         }
-    //         else {
-    //             si=mid+1;
-    //         }
-    //     }
-    //     return firstIndex;
-    // }
-    // public int getLastIndex(int arr[],int target) {
-    //     int n=arr.length;
-    //     int si=0;
-    //     int ei=n-1;
-    //     int lastIndex=-1;
-    //     while(si<=ei) {
-    //         int mid=(si+ei)/2;
-    //         if(arr[mid]==target) {
-    //             lastIndex=mid;
-    //             si=mid+1; // search for some better larger index
-    //         }
-    //         else if(arr[mid]>target) {
-    //             ei=mid-1;
-    //         }
-    //         else {
-    //             si=mid+1;
-    //         }
-    //     }
-    //     return lastIndex;
-    // }
-    // public int[] searchRange(int[] nums, int target) {
-    //     int firstIndex=getFirstIndex(nums,target);
-    //     int lastIndex=getLastIndex(nums,target);
-    //     return new int[]{firstIndex,lastIndex};
-    // }
-    public int getFirstIndex(int arr[],int tar) {
-        int firstIndex=-1;
-        int n=arr.length;
-        int si=0;
-        int ei=n-1;
-        while(si<=ei) {
-            int mid=(si+ei)/2;
-            if(arr[mid]==tar) {
-                firstIndex=mid;
-                ei=mid-1;
-            } 
-            else if(arr[mid]<tar) {
-                si=mid+1;
-            }
-            else {
-                ei=mid-1;
-            }
-        }
-        return firstIndex;
+    public int findFirstIndex(int arr[],int target) {
+           int n=arr.length;
+           int firstIndex=-1;
+           int low=0;
+           int high=n-1;
+           while(low<=high) {
+              int mid=(low+high)/2;
+              if(arr[mid]==target) {
+                  firstIndex=mid;
+                  high=mid-1;
+              }
+              else if(arr[mid]<target) {
+                  low=mid+1;
+              }
+              else {
+                  high=mid-1;
+              }
+           }
+           return firstIndex;
     }
-    public int getLastIndex(int arr[],int tar) {
-        int lastIndex=-1;
-        int n=arr.length;
-        int si=0;
-        int ei=n-1;
-        while(si<=ei) {
-            int mid=(si+ei)/2;
-            if(arr[mid]==tar) {
-                lastIndex=mid;
-                si=mid+1;
-            }
-            else if(arr[mid]<tar) {
-                si=mid+1;
-            }
-            else {
-                ei=mid-1;
-            }
-        }
-        return lastIndex;
+    public int findLastIndex(int arr[],int target) {
+           int n=arr.length;
+           int lastIndex=-1;
+           int low=0;
+           int high=n-1;
+           while(low<=high) {
+               int mid=(low+high)/2;
+               if(arr[mid]==target) {
+                   lastIndex=mid;
+                   low=mid+1;
+               }
+               else if(arr[mid]<target) {
+                  low=mid+1;
+               }
+               else {
+                  high=mid-1;
+               }
+           }
+           return lastIndex;
     }
     public int[] searchRange(int[] nums, int target) {
-       int firstIndex=getFirstIndex(nums,target);
-       int lastIndex=getLastIndex(nums,target);
-       return new int[]{firstIndex,lastIndex};
+        int n=nums.length;
+        int firstIndex=findFirstIndex(nums,target);
+        int lastIndex=findLastIndex(nums,target);
+        return new int[]{firstIndex,lastIndex};
     }
 }
