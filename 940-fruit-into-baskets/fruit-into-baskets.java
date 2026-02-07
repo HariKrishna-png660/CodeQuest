b943eb2dce88305eb8b0c76adbd4ef15ff3bdeb8
HariKrishna-png660 <htalagapu@gmail.com>
2026-02-07 10:56:15 +0530

    Time: 16 ms (90.37%) | Memory: 55.4 MB (99.83%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -1,16 +1,17 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        int n=fruits.length;
-        int si=0;
-        int ei=0;
-        int maxFruits=0;
-        int freq[]=new int[(int)(1e5)+1];
-        int count=0;
-        while(ei<n) {
-            if(freq[fruits[ei]]==0) {
+         int n=fruits.length;
+         int si=0;
+         int ei=0;
+         int maxFruits=0;
+         int freq[]=new int[(int)1e6];
+         int count=0;
+         while(ei<n) {
+            int currFruit=fruits[ei];
+            if(freq[currFruit]==0) {
                 count++;
             }
-            freq[fruits[ei]]++;
+            freq[currFruit]++;
             ei++;
             while(count>2) {
                 if(freq[fruits[si]]==1) {
@@ -20,7 +21,7 @@ class Solution {
                 si++;
             }
             maxFruits=Math.max(maxFruits,ei-si);
-        }
-        return maxFruits;
+         }
+         return maxFruits;
     }
 }
