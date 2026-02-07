72a7724357ce66c858a0390232503853c623f424
HariKrishna-png660 <htalagapu@gmail.com>
2026-02-07 12:21:07 +0530

    Time: 23 ms (93.58%) | Memory: 49.3 MB (11.27%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -2,21 +2,16 @@ class Solution {
     public int subarraySum(int[] nums, int k) {
         int n=nums.length;
         HashMap<Integer,Integer> map=new HashMap<>();
-        int currSum=0;
         map.put(0,1);
-        int count=0;
+        int subarrayCount=0;
+        int currSum=0;
         for(int i=0;i<n;i++) {
             currSum += nums[i];
             if(map.containsKey(currSum-k)) {
-                count += map.get(currSum-k);
+                subarrayCount += map.get(currSum-k);
             }
-            if(map.containsKey(currSum)) {
-                map.put(currSum,map.getOrDefault(currSum,0)+1);
-            }
-            if(!map.containsKey(currSum)) {
-                map.put(currSum,1);
-            }
+           map.put(currSum,map.getOrDefault(currSum,0)+1);
         }
-       return count;
+        return subarrayCount;
     }
 }
