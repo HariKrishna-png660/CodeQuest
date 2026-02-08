b04703630dff1b5fcbb5bc1e2b6ddf5a98e563a9
HariKrishna-png660 <htalagapu@gmail.com>
2026-02-08 22:46:15 +0530

    Time: 6 ms (5.87%) | Memory: 48.3 MB (37.21%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,26 +1,26 @@
 class Solution {
+    // one iteration 
+    // time = O(N)
+    // space =O(N)
     public int trap(int[] height) {
-        // find the left max and right max for every index 
-        // the min of both the max is the capacity of water it can hold
-        // also the we subtract the height of the each bar 
         int n=height.length;
-        int leftMax[]=new int[n];
-        int rightMax[]=new int[n];
-        leftMax[0]=height[0];
-        for(int i=1;i<n;i++) {
-             leftMax[i]=Math.max(leftMax[i-1],height[i-1]);
-        }
-        rightMax[n-1]=height[n-1];
-        for(int i=n-2;i>=0;i--) {
-            rightMax[i]=Math.max(rightMax[i+1],height[i+1]);
-        }
-        int trapWater=0;
-        for(int i=1;i<n-1;i++) {
-            int minHeight=Math.min(rightMax[i],leftMax[i]);
-            if((minHeight-height[i])>0) {
-                trapWater += (minHeight-height[i]);
+        Stack<Integer> st=new Stack<>();
+        int trappedWater=0;
+        for(int i=0;i<n;i++) {
+            while(st.size()>0 && height[st.peek()] < height[i]) {
+                int currentIdx=st.pop();
+                if(st.size()==0) {
+                    break;
+                }
+                int right=i;
+                int left=st.peek();
+                int allowedHeight=Math.min(height[right],height[left]);
+                int h=allowedHeight-height[currentIdx];
+                int w=right-left-1;
+                trappedWater += h*w;
             }
+            st.push(i);
         }
-        return trapWater;
+        return trappedWater;
     }
 }
