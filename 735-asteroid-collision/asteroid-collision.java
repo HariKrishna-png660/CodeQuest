6844fdf1e4099185b2acb1bb52511eec418de350
HariKrishna-png660 <htalagapu@gmail.com>
2026-02-08 12:28:25 +0530

    Time: 4 ms (80.98%) | Memory: 46.9 MB (69.85%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -20,7 +20,7 @@ class Solution {
                 boolean canPush=true;
                 while(st.size()>0 && st.peek()>0) {
                     if(st.peek() < -(currentValue)) {
-                        // canPush=true;
+                        canPush=true;
                         st.pop();
                     }
                     else if(st.peek() == -(currentValue)) {
