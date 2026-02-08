a8a71ebc938b6336d7ee933378b1273822732267
HariKrishna-png660 <htalagapu@gmail.com>
2026-02-08 21:43:36 +0530

    Time: 1 ms (60.72%) | Memory: 49 MB (8.52%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -3,30 +3,24 @@ class Solution {
         // find the left max and right max for every index 
         // the min of both the max is the capacity of water it can hold
         // also the we subtract the height of the each bar 
-        int n = height.length;
-        if(n == 0) return 0;
-
-        int leftMax[] = new int[n];
-        int rightMax[] = new int[n];
-
-        // build left max
-        leftMax[0] = height[0];
-        for(int i=1;i<n;i++){
-            leftMax[i] = Math.max(leftMax[i-1], height[i]);
+        int n=height.length;
+        int leftMax[]=new int[n];
+        int rightMax[]=new int[n];
+        leftMax[0]=height[0];
+        for(int i=1;i<n;i++) {
+             leftMax[i]=Math.max(leftMax[i-1],height[i-1]);
         }
-
-        // build right max
-        rightMax[n-1] = height[n-1];
-        for(int i=n-2;i>=0;i--){
-            rightMax[i] = Math.max(rightMax[i+1], height[i]);
+        rightMax[n-1]=height[n-1];
+        for(int i=n-2;i>=0;i--) {
+            rightMax[i]=Math.max(rightMax[i+1],height[i+1]);
         }
-
-        int water = 0;
-
-        for(int i=0;i<n;i++){
-            water += Math.min(leftMax[i], rightMax[i]) - height[i];
+        int trapWater=0;
+        for(int i=1;i<n-1;i++) {
+            int minHeight=Math.min(rightMax[i],leftMax[i]);
+            if((minHeight-height[i])>0) {
+                trapWater += (minHeight-height[i]);
+            }
         }
-
-        return water;
+        return trapWater;
     }
 }
