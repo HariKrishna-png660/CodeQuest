90869d52eb4e3dce6e5fe805f75080a0f18ccf1e
HariKrishna-png660 <htalagapu@gmail.com>
2026-02-12 22:49:16 +0530

    Time: 1 ms (80.67%) | Memory: 44.7 MB (69.72%) - LeetSync

--- 684-redundant-connection/redundant-connection.java
+++ 684-redundant-connection/redundant-connection.java
@@ -1,30 +1,40 @@
 class Solution {
-    // here i am using dsu with path compression  
+    // this is the solution with only union by size
     int par[];
+    int size[];
     public int findPar(int u) {
         if(par[u]==u) {
             return u;
         }
-        return par[u]=findPar(par[u]);
+        return findPar(par[u]);
     }
     public int[] findRedundantConnection(int[][] edges) {
         int N=edges.length;
-         par=new int[N];
-         for(int i=0;i<N;i++) {
+        par=new int[N];
+        size=new int[N];
+        for(int i=0;i<N;i++) {
             par[i]=i;
-         }
-         for(int i=0;i<N;i++) {
-            int u=edges[i][0]-1;
-            int v=edges[i][1]-1;
+            size[i]=1;
+        }
+        for(int edge[]:edges) {
+            int u=edge[0]-1;
+            int v=edge[1]-1;
             int p1=findPar(u);
             int p2=findPar(v);
-            if(p1==p2) {
-                return new int[]{u+1,v+1};
+            if(p1!=p2) {
+                if(size[p1]<size[p2]) {
+                    par[p1]=p2;
+                    size[p2] += size[p1];
+                }
+                else {
+                    par[p2]=p1;
+                    size[p1] += size[p2];
+                }
             }
             else {
-                par[p2]=p1;
+                return new int[]{u+1,v+1};
             }
-         }
-         return new int[]{0,0};
+        } 
+        return new int[]{0,0};
     }
 }
