80718c23987a865829a770953135acc169e2ddf3
HariKrishna-png660 <htalagapu@gmail.com>
2026-02-13 17:44:26 +0530

    Time: 3 ms (28.60%) | Memory: 46.6 MB (67.89%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,30 +1,55 @@
 class Solution {
-    public int areaOfIsland(int grid[][],boolean vis[][],int row,int col) {
-        if(row<0 || row>=grid.length || col<0 || col>=grid[0].length || grid[row][col]==0 || vis[row][col]) {
-            return 0;
-        }
-        vis[row][col]=true;
-        int dirs[][]={{-1,0},{0,-1},{1,0},{0,1}};
-        int area=0;
-        for(int dir[]:dirs) {
-            int x=row+dir[0];
-            int y=col+dir[1];
-            area += areaOfIsland(grid,vis,x,y);
-        }
-        return area+1;
+    int par[];
+    int size[];
+    public int findPar(int u) {
+         if(par[u]==u) {
+            return u;
+         }
+         return par[u]=findPar(par[u]);
     }
     public int maxAreaOfIsland(int[][] grid) {
-        int n=grid.length;
-        int m=grid[0].length;
-        int maxArea=Integer.MIN_VALUE;
-        boolean vis[][]=new boolean[n][m];
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<m;j++) {
-                if(grid[i][j]==1 && !vis[i][j]) {
-                     maxArea=Math.max(maxArea,areaOfIsland(grid,vis,i,j));
-                }
+         int m=grid.length;
+         int n=grid[0].length;
+         par=new int[m*n];
+         size=new int[m*n];
+         // initialise the parent and size array
+         for(int i=0;i<m;i++) { 
+            for(int j=0;j<n;j++) {
+              if(grid[i][j]==1) {
+                 par[i*n+j]=i*n+j;
+                 size[i*n+j]=1;
+              }
+              else {
+                 par[i*n+j]=-1;
+                 size[i*n+j]=0;
+              }
             }
-        }
-        return maxArea==Integer.MIN_VALUE ? 0:maxArea;
+         }
+         int dirs[][]={{-1,0},{0,-1},{1,0},{0,1}};
+         // traverse in the grid
+         for(int i=0;i<m;i++) {
+             for(int j=0;j<n;j++) {
+                if(grid[i][j]==1) {
+                    int p1=findPar(i*n+j);
+                // check the parents of the neighbours of (x,y) and then assign the parents of the neighbours as the parent of (x,y)
+                for(int dir[]:dirs) {
+                    int x=dir[0]+i;
+                    int y=dir[1]+j;
+                    if(x>=0 && x<m && y>=0 && y<n && grid[x][y]==1) {
+                        int p2=findPar(x*n+y);
+                        if(p1!=p2) {
+                            par[p2]=p1;
+                            size[p1] += size[p2];
+                        }
+                    }
+                }
+                }
+             }
+         }
+         int maxArea=0;
+         for(int i=0;i<n*m;i++) {
+            maxArea=Math.max(maxArea,size[i]);
+         }
+         return maxArea;
     }
 }
