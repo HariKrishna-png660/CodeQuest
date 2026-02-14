ff119020531adad8a1b6f54d1c1c0c7e25603785
HariKrishna-png660 <htalagapu@gmail.com>
2026-02-14 17:51:00 +0530

    Time: 51 ms (6.48%) | Memory: 93 MB (23.70%) - LeetSync

--- 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.java
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.java
@@ -1,26 +1,29 @@
 class Solution {
     public boolean canArrange(int[] arr, int k) {
-        int n=arr.length;
-        HashMap<Integer,Integer> map=new HashMap<>();
-        int count=0;
-        for(int i=0;i<n;i++) {
-            int rem=arr[i]%k;
-            rem=(rem+k)%k;
-            int reqRem=(k-rem)%k;
-            if(map.containsKey(reqRem)) {
-                count++;
-                int val=map.get(reqRem);
+         int n=arr.length;
+         HashMap<Integer,Integer> map=new HashMap<>();
+         int pairs=0;
+         for(int i=0;i<n;i++) {
+            int rem=(arr[i]%k+k)%k;
+            int req=((k-rem)+k)%k;
+            if(map.containsKey(req)) {
+                pairs++;
+                int val=map.get(req);
                 if(val==1) {
-                   map.remove(reqRem);
-                } 
+                    map.remove(req);
+                }
                 else {
-                    map.put(reqRem,val-1);
+                    map.put(req,map.getOrDefault(req,0)-1);
                 }
             }
             else {
-                 map.put(rem,map.getOrDefault(rem,0)+1);
+                map.put(rem,map.getOrDefault(rem,0)+1);
             }
-        }
-       return count==n/2;
+         }
+         System.out.println(pairs);
+         if(pairs==n/2) {
+            return true;
+         }
+         return false;
     }
 }
