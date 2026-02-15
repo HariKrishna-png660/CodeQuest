60a427f8ae4ee681a06a86497938b5cbe7d5255f
HariKrishna-png660 <htalagapu@gmail.com>
2026-02-15 16:22:17 +0530

    Time: 146 ms (23.40%) | Memory: 48.4 MB (7.49%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,38 +1,28 @@
 class Solution {
-    public void fillValues(boolean dp[][],String s) {
-        int n=dp.length;
-        for(int diag=0;diag<n;diag++) {
-            for(int i=0,j=diag;j<n;i++,j++) {
-                if(diag==0) {
+    public String longestPalindrome(String s) {
+        int n=s.length();
+        boolean dp[][]=new boolean[n][n];
+        int maxLen=0;
+        int si=0;
+        for(int gap=0;gap<n;gap++) {
+            for(int i=0,j=gap;j<n;i++,j++) {
+                if(gap==0) {
                     dp[i][j]=true;
                 }
-                else if(diag==1) {
-                     if(s.charAt(i)==s.charAt(j)) {
-                        dp[i][j]=true;
-                     }
+                else if(gap==1) {
+                    dp[i][j]= (s.charAt(i)==s.charAt(j));
                 }
                 else {
                     if(s.charAt(i)==s.charAt(j)) {
                         dp[i][j]=dp[i+1][j-1];
                     }
                 }
+                if(dp[i][j]==true && maxLen<j-i+1) {
+                    maxLen=j-i+1;
+                    si=i;
+                }
             }
         }
-    }
-    public String longestPalindrome(String s) {
-         int n=s.length();
-         boolean dp[][]=new boolean[n][n];
-         fillValues(dp,s);
-         int maxLen=0;
-         int si=0;
-         for(int i=0;i<n;i++) {
-             for(int j=i;j<n;j++) {
-                 if(dp[i][j] && maxLen < j-i+1) {
-                      maxLen=j-i+1;
-                      si=i;
-                 }
-             }
-         }
-         return s.substring(si,si+maxLen);
+        return s.substring(si,si+maxLen);
     }
 }
