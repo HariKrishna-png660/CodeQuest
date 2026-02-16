68a9e1120edd9b4ba9b280f92a32a0ed7b313b9c
HariKrishna-png660 <htalagapu@gmail.com>
2026-02-16 08:30:48 +0530

    Time: 6 ms (10.12%) | Memory: 47.4 MB (16.37%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,32 +1,24 @@
 class Solution {
-    public int minDistance(String word1,String word2,int n,int m,int dp[][]) {
-         if(n==0) {
-            return m;
-         }
-         if(m==0) {
-            return n;
-         }
-         if(dp[n][m]!=-1) {
-            return dp[n][m];
-         }
-         if(word1.charAt(n-1)==word2.charAt(m-1)) {
-            return dp[n][m]=minDistance(word1,word2,n-1,m-1,dp);
-         }
-         int insert=minDistance(word1,word2,n,m-1,dp);
-         int delete=minDistance(word1,word2,n-1,m,dp);
-         int replace=minDistance(word1,word2,n-1,m-1,dp);
-         return dp[n][m]=Math.min(insert,Math.min(delete,replace))+1;
-    }
     public int minDistance(String word1, String word2) {
-        // insert -> (i,j-1)
-        // delete -> (i-1,j)
-        // replace -> (i-1,j-1)
         int n=word1.length();
         int m=word2.length();
         int dp[][]=new int[n+1][m+1];
-        for(int i=0;i<=n;i++) {
-            Arrays.fill(dp[i],-1);
+        for(int j=0;j<=m;j++) {
+            for(int i=0;i<=n;i++) {
+                if(i==0) {
+                    dp[i][j]=j;  // when first word is empty and second word contains some letters 
+                    continue;
+                }
+                if(j==0) {
+                    dp[i][j]=i; // when second word is empty and first word contains some letters
+                    continue;
+                }
+                dp[i][j]=dp[i-1][j-1];
+                if(word1.charAt(i-1)!=word2.charAt(j-1)) {
+                    dp[i][j]=Math.min(dp[i][j],Math.min(dp[i][j-1],dp[i-1][j]))+1;
+                }
+            }
         }
-        return minDistance(word1,word2,n,m,dp);
+        return dp[n][m];
     }
 }
