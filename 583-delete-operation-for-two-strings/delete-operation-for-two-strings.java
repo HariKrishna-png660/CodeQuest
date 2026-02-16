52c1aba6a69b8816849cbe3729ab682e22c9f260
HariKrishna-png660 <htalagapu@gmail.com>
2026-02-16 10:18:38 +0530

    Time: 7 ms (71.95%) | Memory: 46.2 MB (90.78%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -1,27 +1,24 @@
 class Solution {
-    public int minDistance(String word1,String word2,int n,int m,int dp[][]) {
-        if(n==0 || m==0) {
-            return n==0?m:n;
-        }
-        if(dp[n][m]!=-1) {
-            return dp[n][m];
-        }
-        int res=0;
-        if(word1.charAt(n-1)==word2.charAt(m-1)) {
-            res=minDistance(word1,word2,n-1,m-1,dp);
-        }
-        else {
-            res=Math.min(minDistance(word1,word2,n-1,m,dp),minDistance(word1,word2,n,m-1,dp))+1;
-        }
-        return dp[n][m]=res;
-    }
     public int minDistance(String word1, String word2) {
-         int n=word1.length();
-         int m=word2.length();
-         int dp[][]=new int[n+1][m+1];
-         for(int i=0;i<=n;i++) {
-            Arrays.fill(dp[i],-1);
-         }
-         return minDistance(word1,word2,n,m,dp);
+        int n=word1.length();
+        int m=word2.length();
+        int dp[][]=new int[n+1][m+1];
+        for(int j=0;j<=m;j++) {
+            for(int i=0;i<=n;i++) {
+                if(i==0) {
+                    dp[i][j]=j;
+                }
+                else if(j==0) {
+                    dp[i][j]=i;
+                }
+                else if(word1.charAt(i-1)==word2.charAt(j-1)) {
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else {
+                    dp[i][j]=Math.min(dp[i-1][j],dp[i][j-1])+1;
+                }
+            }
+        }
+        return dp[n][m];
     }
 }
