441176b1b1652a8aa13ef9e0dce12b8c78f1ef9d
HariKrishna-png660 <htalagapu@gmail.com>
2026-02-16 16:49:21 +0530

    Time: 18 ms (95.02%) | Memory: 45.5 MB (75.41%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.java
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.java
@@ -1,33 +1,40 @@
 class Solution {
     public int findNumberOfLIS(int[] nums) {
+        // intially the value in the dp and count array are 1 
+        // main logic is at index j we need to find out the count of lis 
+        // 0 1 2 3 ..... j-2 j-1 j
+        // let say indices 2 4 7 9 11 are those which are less than the current idx value
+        // THEN STARTING FROM THE INDEX 11 TILL 2 WILL CHECK FOR THE LIS
+        // IF THE LENGTH OF LIS IS LARGER THAN THE PRESENT THEN INITILISE THE LIS LENGTH WITH THAT 
+        // IF THE LIS LENGTH IS EQUAL THEN ADD THE COUNT OF THE LIS 
         int n=nums.length;
-        int count[]=new int[n];
         int dp[]=new int[n];
-        Arrays.fill(count,1);
+        int count[]=new int[n];
         Arrays.fill(dp,1);
+        Arrays.fill(count,1);
         for(int i=1;i<n;i++) {
             for(int j=i-1;j>=0;j--) {
                 if(nums[j]<nums[i]) {
-                     if(dp[i]<dp[j]+1) {
-                        dp[i]=dp[j]+1;
-                        count[i]=count[j];
-                     }
-                     else if(dp[i]==dp[j]+1) {
-                        count[i] += count[j];
-                     }
-                }
+                 if(dp[i]<dp[j]+1) {
+                    dp[i]=dp[j]+1;
+                    count[i]=count[j];
+                 }
+                 else if(dp[i]==dp[j]+1) {
+                    count[i] += count[j];
+                 }
+              }
             }
         }
-        int maxLen=Integer.MIN_VALUE;
+        int maxLen=0;
         for(int i=0;i<n;i++) {
             maxLen=Math.max(maxLen,dp[i]);
         }
-        int ans=0;
+        int numberOfLis=0;
         for(int i=0;i<n;i++) {
             if(dp[i]==maxLen) {
-                ans += count[i];
+                numberOfLis += count[i];
             }
         }
-        return ans;
+        return numberOfLis;
     }
 }
