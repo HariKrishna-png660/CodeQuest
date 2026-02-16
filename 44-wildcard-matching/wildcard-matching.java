03733394dafe22ec8469be44014a7cce2446af54
HariKrishna-png660 <htalagapu@gmail.com>
2026-02-16 09:21:44 +0530

    Time: 15 ms (73.00%) | Memory: 47.6 MB (59.72%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,32 +1,26 @@
 class Solution {
-    public boolean isMatch(int n,int m,String s,String p,Boolean dp[][]) {
-         if(n==0 || m==0) {
-              if(n==0 && m==0) {
-                 return dp[n][m]=true;
-              }
-              else if(m==1 && p.charAt(m-1)=='*') {
-                 return dp[n][m]=true;
-              }
-              return dp[n][m]=false;
-         }
-         if(dp[n][m]!=null) {
-             return dp[n][m];
-         }
-         boolean res=false;
-         if(s.charAt(n-1)==p.charAt(m-1) || p.charAt(m-1)=='?') {
-            res=isMatch(n-1,m-1,s,p,dp);
-         }
-         else if(p.charAt(m-1)=='*') {
-            res=isMatch(n-1,m,s,p,dp) || isMatch(n,m-1,s,p,dp);
-         }
-         return dp[n][m]=res;
+    public boolean isMatch_rec(String s,String p,int n,int m,Boolean dp[][]) {
+        if(n==0 || m==0) {
+            if(n==0 && m==0)  return dp[n][m]=true;
+            else if(m==1 && p.charAt(m-1)=='*') return dp[n][m]=true;
+            return dp[n][m]=false;
+        }
+        if(dp[n][m]!=null)  return dp[n][m];
+        boolean res=false;
+        if((s.charAt(n-1) == p.charAt(m-1)) || p.charAt(m-1) == '?') {
+            res=isMatch_rec(s,p,n-1,m-1,dp);
+        }
+        else if(p.charAt(m-1)=='*') {
+            res= isMatch_rec(s,p,n-1,m,dp) || isMatch_rec(s,p,n,m-1,dp);
+        }
+        return dp[n][m]=res;
     }
-    public String removeConsecutiveStars(String s) {
-        int n=s.length();
+    public String removeConsecutiveStars(String p) {
+        int n=p.length();
         StringBuilder sb=new StringBuilder();
         for(int i=0;i<n;i++) {
-            char ch=s.charAt(i);
-            while(i<s.length() && s.charAt(i)=='*') {
+            char ch=p.charAt(i);
+            while(i<p.length() && p.charAt(i)=='*') {
                 i++;
             }
             sb.append(ch);
@@ -40,7 +34,8 @@ class Solution {
         p=removeConsecutiveStars(p);
         int n=s.length();
         int m=p.length();
+        // System.out.println(p);
         Boolean dp[][]=new Boolean[n+1][m+1];
-        return isMatch(n,m,s,p,dp);
+        return isMatch_rec(s,p,n,m,dp);
     }
 }
