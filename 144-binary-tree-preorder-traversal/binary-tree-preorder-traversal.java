4bc00d8283032d0b3fe6f9b16090ba808fdb4fe4
HariKrishna-png660 <htalagapu@gmail.com>
2026-02-19 23:52:48 +0530

    Time: 1 ms (7.47%) | Memory: 43.3 MB (51.06%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -14,17 +14,38 @@
  * }
  */
 class Solution {
-    public void preorderTraversal(TreeNode root,List<Integer> ans) {
-        if(root==null) {
-            return;
+    class Pair {
+        TreeNode node;
+        int state;
+        public Pair(TreeNode node,int state) {
+            this.node=node;
+            this.state=state;
         }
-        ans.add(root.val);
-        preorderTraversal(root.left,ans);
-        preorderTraversal(root.right,ans);
     }
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> preorder=new ArrayList<>();
-         preorderTraversal(root,preorder);
-         return preorder;
+        Stack<Pair> st=new Stack<>();
+        List<Integer> ans=new ArrayList<>();
+        if(root!=null) {
+            st.add(new Pair(root,1));
+        }
+        while(st.size()>0) {
+            if(st.peek().state==1) {
+                ans.add(st.peek().node.val);
+                st.peek().state++;
+                if(st.peek().node.left!=null) {
+                     st.push(new Pair(st.peek().node.left,1));
+                }
+            }
+            else if(st.peek().state==2) {
+                st.peek().state++;
+                if(st.peek().node.right!=null) {
+                     st.push(new Pair(st.peek().node.right,1));
+                }
+            }
+            else {
+                st.pop();
+            }
+        }
+        return ans;
     }
 }
