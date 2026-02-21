82552fa0644d73af609f0287c768e6dcd6734573
HariKrishna-png660 <htalagapu@gmail.com>
2026-02-21 10:40:49 +0530

    Time: 7 ms (98.74%) | Memory: 46.6 MB (42.14%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -8,43 +8,18 @@
  * }
  */
 class Solution {
-    public ArrayList<TreeNode> nodeToRootPath(TreeNode root,TreeNode node) {
-        if(root==null) {
-            return new ArrayList<>();
-        }
-        if(root.equals(node)) {
-            ArrayList<TreeNode> baseAns=new ArrayList<>();
-            baseAns.add(root);
-            return baseAns;
-        }
-        ArrayList<TreeNode> left=nodeToRootPath(root.left,node);
-        if(!left.isEmpty()) {
-            left.add(root);
-            return left;
-        }
-        ArrayList<TreeNode> right=nodeToRootPath(root.right,node);
-        if(!right.isEmpty()) {
-            right.add(root);
-            return right;
-        }
-        return new ArrayList<>();
-    }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        ArrayList<TreeNode> ntr1=nodeToRootPath(root,p);
-        ArrayList<TreeNode> ntr2=nodeToRootPath(root,q);
-        int i=ntr1.size()-1;
-        int j=ntr2.size()-1;
-        TreeNode lca=root;
-        while(i>=0 && j>=0) {
-            if(ntr1.get(i).equals(ntr2.get(j))) {
-               lca=ntr1.get(i);
-            }
-            else {
-                break;
-            }
-            i--;
-            j--;
+        if(root==null) {
+            return root;
         }
-        return lca;
+        if(root.equals(p) || root.equals(q)) {
+            return root;
+        }
+        TreeNode left=lowestCommonAncestor(root.left,p,q);
+        TreeNode right=lowestCommonAncestor(root.right,p,q);
+        if(left!=null && right!=null) {
+            return root;
+        }
+        return left==null?right:left;
     }
 }
