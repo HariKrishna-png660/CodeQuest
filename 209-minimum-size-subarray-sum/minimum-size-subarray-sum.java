c4c29512cf7904f871edfa9eb3434e3a0f17373e
HariKrishna-png660 <htalagapu@gmail.com>
2026-02-23 16:34:09 +0530

    Time: 1 ms (99.73%) | Memory: 61.9 MB (86.88%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -1,36 +1,23 @@
 class Solution {
-    public boolean isPossible(int nums[],int len,int target) {
-        int n=nums.length;
-        int sum=0;
-        for(int i=0;i<len;i++) {
-            sum += nums[i];
-        }
-        if(sum >=target) {
-            return true;
-        }
-        for(int i=len;i<n;i++) {
-            sum += nums[i] -nums[i-len];
-            if(sum>=target) {
-                return true;
-            }
-        }
-        return false;
-    }
     public int minSubArrayLen(int target, int[] nums) {
         int n=nums.length;
-        int low=1;
-        int high=n;
-        int ans=0;
-        while(low<=high) {
-            int mid=(low+high)/2;
-            if(isPossible(nums,mid,target)) {
-                ans=mid;
-                high=mid-1;
+        int si=0;
+        int ei=0;
+        int sum=0;
+        int minLen=n+1;
+        while(ei<n) {
+            sum += nums[ei];
+            ei++;
+            while(sum>target) {
+                 if(sum>target) {
+                minLen=Math.min(minLen,ei-si);
+               }
+                sum -= nums[si++];
             }
-            else {
-                low=mid+1;
+            if(sum>=target) {
+                minLen=Math.min(minLen,ei-si);
             }
         }
-       return ans;
+        return minLen==n+1?0:minLen;
     }
 }
