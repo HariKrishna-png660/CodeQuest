540de410e3f253d619ac8d21b0b116c6155eeeae
HariKrishna-png660 <htalagapu@gmail.com>
2026-02-26 18:13:54 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (84.52%) - LeetSync

--- 1065-binary-string-with-substrings-representing-1-to-n/binary-string-with-substrings-representing-1-to-n.java
+++ 1065-binary-string-with-substrings-representing-1-to-n/binary-string-with-substrings-representing-1-to-n.java
@@ -1,20 +1,12 @@
 class Solution {
-    public boolean queryString(String s, int n) {
-        int k=s.length();
-        HashSet<String> set=new HashSet<>();
-        for(int i=0;i<k;i++) {
-            for(int j=i+1;j<=k;j++) {
-                String sub=s.substring(i,j);
-                set.add(sub);
+    public boolean queryString(String s, int m) {
+        int n=s.length();
+        for(int i=1;i<=m;i++) {
+            String str=Integer.toBinaryString(i);
+            if(!s.contains(str)) {
+                return false;
             }
         }
-        for(int i=1;i<=n;i++) {
-            String str=Integer.toBinaryString(i);
-            if(!set.contains(str)) {
-                return false;
-            } 
-        }
-        // System.out.println(set);
         return true;
     }
 }
