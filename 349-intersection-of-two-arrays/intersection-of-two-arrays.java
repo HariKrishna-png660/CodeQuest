a5151c9f5320584aff91189453393813610ca2ae
HariKrishna-png660 <htalagapu@gmail.com>
2026-03-09 20:58:36 +0530

    Time: 4 ms (35.81%) | Memory: 44.8 MB (87.13%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,22 +1,30 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        int n=nums1.length;
-        int m=nums2.length;
-        HashMap<Integer,Integer> map1=new HashMap<>();
-        for(int i=0;i<n;i++) {
-            map1.put(nums1[i],map1.getOrDefault(nums1[i],0)+1);
-        }
-        HashMap<Integer,Integer> ans=new HashMap<>();
-        for(int i=0;i<m;i++) {
-            if(map1.containsKey(nums2[i]) && !ans.containsKey(nums2[i])) {
-                ans.put(nums2[i],1);
-            } 
-        }
-        int res[]=new int[ans.size()];
-        int i=0;
-        for(int key:ans.keySet()) {
-            res[i++]=key;
-        }
-        return res;
+         int n=nums1.length;
+         int m=nums2.length;
+         HashMap<Integer,Integer> map1=new HashMap<>();
+         HashMap<Integer,Integer> map2=new HashMap<>();
+         for(int i=0;i<n;i++) {
+            int val=nums1[i];
+            map1.put(val,map1.getOrDefault(val,0)+1);
+         }
+         for(int i=0;i<m;i++) {
+            int val=nums2[i];
+            map2.put(val,map2.getOrDefault(val,0)+1);
+         }
+         // iterate on any one of the hashMap 
+         ArrayList<Integer> list=new ArrayList<>();
+         for(int key:map1.keySet()) {
+              if(map2.containsKey(key)) {
+                 list.add(key);
+              }
+         }
+         int size=list.size();
+         int ans[]=new int[size];
+         int i=0;
+         for(int val:list) {
+            ans[i++]=val;
+         }
+         return ans;
     }
 }
