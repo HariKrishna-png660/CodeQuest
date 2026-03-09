3c41e6a0e2cdbdfdee16ee416f32f3f80b5ce64c
HariKrishna-png660 <htalagapu@gmail.com>
2026-03-09 21:28:36 +0530

    Time: 7 ms (30.42%) | Memory: 47 MB (96.30%) - LeetSync

--- 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.java
+++ 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.java
@@ -1,35 +1,32 @@
 class Solution {
     public int[] findIntersectionValues(int[] nums1, int[] nums2) {
-        int n=nums1.length;
-        int m=nums2.length;
-        // store both nums1 and nums2 in hash map
+        int ans[]=new int[2];
         HashMap<Integer,Integer> map1=new HashMap<>();
         HashMap<Integer,Integer> map2=new HashMap<>();
+        int n=nums1.length;
+        int m=nums2.length;
         for(int i=0;i<n;i++) {
-            int value=nums1[i];
-            map1.put(value,map1.getOrDefault(value,0)+1);
+            int val=nums1[i];
+            map1.put(val,map1.getOrDefault(val,0)+1);
         }
         for(int i=0;i<m;i++) {
-            int value=nums2[i];
-            map2.put(value,map2.getOrDefault(value,0)+1);
+            int val=nums2[i];
+            map2.put(val,map2.getOrDefault(val,0)+1);
         }
-        // iterate on nums1 and track common elements from nums2
-        int common1=0;
+        // iterate on nums1 
         for(int i=0;i<n;i++) {
-            int value=nums1[i];
-            if(map2.containsKey(value)) {
-                int val=map2.get(value);
-                common1++;
+            int val=nums1[i];
+            if(map2.containsKey(val)) {
+                ans[0]++;
             }
         }
-        int common2=0;
+        // iterate on nums2
         for(int i=0;i<m;i++) {
-            int value=nums2[i];
-            if(map1.containsKey(value)) {
-                int val=map1.get(value);
-                common2++;
+            int val=nums2[i];
+            if(map1.containsKey(val)) {
+                ans[1]++;
             }
         }
-        return new int[]{common1,common2};
+        return ans;
     }
 }
