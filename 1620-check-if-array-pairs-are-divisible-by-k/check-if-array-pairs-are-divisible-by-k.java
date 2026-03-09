254a10902e9da3f656a86669dd31e766af21ff4d
HariKrishna-png660 <htalagapu@gmail.com>
2026-03-09 21:49:24 +0530

    Time: 42 ms (32.50%) | Memory: 91.1 MB (41.36%) - LeetSync

--- 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.java
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.java
@@ -1,29 +1,30 @@
 class Solution {
     public boolean canArrange(int[] arr, int k) {
-         int n=arr.length;
-         HashMap<Integer,Integer> map=new HashMap<>();
-         int pairs=0;
-         for(int i=0;i<n;i++) {
-            int rem=(arr[i]%k+k)%k;
-            int req=((k-rem)+k)%k;
-            if(map.containsKey(req)) {
-                pairs++;
-                int val=map.get(req);
-                if(val==1) {
+          // ((num % k) + k)%k -> curr
+          // (k-curr)%k  -> req %k is specially for case when curr is zero 
+          int n=arr.length;
+          HashMap<Integer,Integer> map=new HashMap<>();
+          int count=0;
+          for(int i=0;i<n;i++) {
+              int curr=((arr[i]%k)+k)%k;
+              int req=(k-curr)%k;
+            //   System.out.println(curr+" "+req);
+              if(map.containsKey(req)) {
+                count++;
+                 int val=map.get(req);
+                 if(val==1) {
                     map.remove(req);
-                }
-                else {
-                    map.put(req,map.getOrDefault(req,0)-1);
-                }
-            }
-            else {
-                map.put(rem,map.getOrDefault(rem,0)+1);
-            }
-         }
-         System.out.println(pairs);
-         if(pairs==n/2) {
-            return true;
-         }
-         return false;
+                 }
+                 else {
+                    map.put(req,val-1);
+                 }
+              }
+              else {
+                 map.put(curr,map.getOrDefault(curr,0)+1);
+              }
+            //   System.out.println(count);
+          }
+        //   System.out.println(count);
+         return count==n/2;
     }
 }
