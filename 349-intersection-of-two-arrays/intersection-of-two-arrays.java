0244ef9c9c2dec746cbc290efa9f58304f6e1a6d
HariKrishna-png660 <htalagapu@gmail.com>
2026-03-09 21:08:00 +0530

    Time: 3 ms (52.39%) | Memory: 45.1 MB (38.52%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,30 +1,31 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-         int n=nums1.length;
-         int m=nums2.length;
-         HashMap<Integer,Integer> map1=new HashMap<>();
-         HashMap<Integer,Integer> map2=new HashMap<>();
-         for(int i=0;i<n;i++) {
+        int n=nums1.length;
+        int m=nums2.length;
+        HashSet<Integer> set1=new HashSet<>();
+        HashSet<Integer> set2=new HashSet<>();
+        for(int i=0;i<n;i++) {
             int val=nums1[i];
-            map1.put(val,map1.getOrDefault(val,0)+1);
-         }
-         for(int i=0;i<m;i++) {
+            set1.add(val);
+        }
+        for(int i=0;i<m;i++) {
             int val=nums2[i];
-            map2.put(val,map2.getOrDefault(val,0)+1);
-         }
-         // iterate on any one of the hashMap 
-         ArrayList<Integer> list=new ArrayList<>();
-         for(int key:map1.keySet()) {
-              if(map2.containsKey(key)) {
-                 list.add(key);
-              }
-         }
-         int size=list.size();
-         int ans[]=new int[size];
-         int i=0;
-         for(int val:list) {
-            ans[i++]=val;
-         }
-         return ans;
+            set2.add(val);
+        }
+        // iterate on the values in any set 
+        // check whether the value is present in the other set 
+        // if yes then add to the arraylist
+        ArrayList<Integer> list=new ArrayList<>();
+        for(int val:set1) {
+            if(set2.contains(val)) {
+                list.add(val);
+            }
+        }
+        int size=list.size();
+        int ans[]=new int[size];
+        for(int i=0;i<size;i++) {
+            ans[i]=list.get(i);
+        }
+        return ans;
     }
 }
