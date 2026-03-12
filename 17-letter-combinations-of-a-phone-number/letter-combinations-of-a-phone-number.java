fbd1ac65d933982db1c66fe07426d13bf78d73db
HariKrishna-png660 <htalagapu@gmail.com>
2026-03-13 00:46:30 +0530

    Time: 4 ms (7.02%) | Memory: 46.7 MB (28.27%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,22 +1,22 @@
 class Solution {
     static String letters[]={"/",".","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-    public void letterCombinations(String digits,String sub,List<String> ans) {
-        if(digits.length()==0) {
-            ans.add(sub);
-            return;
-        }
-        char firstChar=digits.charAt(0);
-        String remString=digits.substring(1);
-        int index=firstChar-'0';
-        String letter=letters[index];
-        for(int i=0;i<letter.length();i++) {
-            char ch=letter.charAt(i);
-            letterCombinations(remString,sub+ch,ans);
-        }
-    }
     public List<String> letterCombinations(String digits) {
-        List<String> ans=new ArrayList<>();
-        letterCombinations(digits,"",ans);
-        return ans;
+        if(digits.length()==0) {
+            List<String> baseAns=new ArrayList<>();
+            baseAns.add("");
+            return baseAns;
+        }
+        int firstValue=digits.charAt(0)-'0';
+        String currString=letters[firstValue];
+        String remString=digits.substring(1);
+        List<String> myAns=new ArrayList<>();
+        List<String> smallAns=letterCombinations(remString);
+        for(int i=0;i<currString.length();i++) {
+            char ch=currString.charAt(i);
+            for(String s:smallAns) {
+                myAns.add(ch+s);
+            }
+        }
+        return myAns;
     }
 }
