20cb533f697f5dd50bc3223935dd9b681c59a252
HariKrishna-png660 <htalagapu@gmail.com>
2026-03-14 13:38:15 +0530

    Time: 47 ms (98.38%) | Memory: 52.2 MB (95.47%) - LeetSync

--- 1582-design-browser-history/design-browser-history.java
+++ 1582-design-browser-history/design-browser-history.java
@@ -1,36 +1,36 @@
 class BrowserHistory {
+    ListNode curr;
     class ListNode {
         String data;
         ListNode next;
         ListNode prev;
-        public ListNode(String data) {
+        public ListNode(String data){
             this.data=data;
         }
     }
-    ListNode currNode;
     public BrowserHistory(String homepage) {
-        currNode=new ListNode(homepage);
+        curr=new ListNode(homepage);
     }
     
     public void visit(String url) {
         ListNode newNode=new ListNode(url);
-        currNode.next=newNode;
-        newNode.prev=currNode;
-        currNode=newNode;
+        curr.next=newNode;
+        newNode.prev=curr;
+        curr=newNode;
     }
     
     public String back(int steps) {
-        while(currNode.prev!=null && steps-->0) {
-            currNode=currNode.prev;
+        while(curr.prev != null && steps-- > 0) {
+            curr=curr.prev;
         }
-        return currNode.data;
+        return curr.data;
     }
     
     public String forward(int steps) {
-        while(currNode.next!=null && steps-->0) {
-            currNode=currNode.next;
+        while(curr.next != null && steps-- > 0) {
+            curr=curr.next;
         }
-        return currNode.data;
+        return curr.data;
     }
 }
 
