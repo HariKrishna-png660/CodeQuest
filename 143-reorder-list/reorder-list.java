eb2365677cd8eaaf07b49ef7c4a7799eeabaf59a
HariKrishna-png660 <htalagapu@gmail.com>
2026-03-16 15:18:49 +0530

    Time: 3 ms (15.16%) | Memory: 49.5 MB (20.72%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -9,38 +9,12 @@
  * }
  */
 class Solution {
-    public void undo(ListNode head) {
-        ListNode evenPosDummy=new ListNode(-1);
-        ListNode oddPosDummy=new ListNode(-1);
-        ListNode evenCurr=evenPosDummy;
-        ListNode oddCurr=oddPosDummy;
-        ListNode curr=head;
-        int pos=0;
-        while(curr!=null) {
-           ListNode nextNode=curr.next;
-           curr.next=null;
-           if(pos%2==0) {
-              evenCurr.next=curr;
-              evenCurr=evenCurr.next;
-           }
-           else {
-              oddCurr.next=curr;
-              oddCurr=oddCurr.next;
-           }
-           curr=nextNode;
-           pos++;
-        }
-       ListNode oddPosHead=oddPosDummy.next;
-       oddPosDummy.next=null;
-       ListNode midNext=reverse(oddPosHead);
-       evenCurr.next=midNext;
-    }
     public ListNode getMid(ListNode head) {
         ListNode slow=head;
         ListNode fast=head;
         while(fast.next!=null && fast.next.next!=null) {
-            fast=fast.next.next;
             slow=slow.next;
+            fast=fast.next.next;
         }
         return slow;
     }
@@ -48,34 +22,34 @@ class Solution {
         ListNode prev=null;
         ListNode curr=head;
         while(curr!=null) {
-            ListNode nextNode=curr.next;
+            ListNode nextNode = curr.next;
             curr.next=prev;
             prev=curr;
             curr=nextNode;
         }
         return prev;
     }
-    public void reorderList1(ListNode head) {
+    public void reorderList(ListNode head) {
         ListNode mid=getMid(head);
         ListNode nextNode=mid.next;
-        mid.next=null;
-        ListNode firstHead=head;
         ListNode secondHead=reverse(nextNode);
-        ListNode temp1=firstHead;
+        ListNode temp1=head;
         ListNode temp2=secondHead;
-        while(temp2!=null){
+        // System.out.println(temp1.val+ " "+temp2.val);
+        ListNode curr=temp1;
+        // System.out.println("hello");
+        while(temp2 != null) {
             ListNode nextNode1=temp1.next;
-            temp1.next=temp2;
+            curr.next=null;
+            curr.next=temp2;
             temp1=nextNode1;
+            curr=curr.next;
             ListNode nextNode2=temp2.next;
-            temp2.next=temp1;
+            curr.next=null;
+            curr.next=temp1;
             temp2=nextNode2;
+            curr=curr.next;
         }
-
-    }
-     public void reorderList(ListNode head) {
-        reorderList1(head);
-        undo(head);
-        reorderList1(head);
+        curr.next=null;
     }
 }
