d5a0d60ce490769c0b2bb2dbde76f830ab0447f2
HariKrishna-png660 <htalagapu@gmail.com>
2026-03-16 14:57:01 +0530

    Time: 5 ms (46.11%) | Memory: 93.8 MB (97.15%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -9,11 +9,6 @@
  * }
  */
 class Solution {
-    // find the mid and after that we need to split the linkedlist into two parts 
-    // 0 to mid -> 1 part and mid+1 to end -> 2 part
-    // compare each of the corresponding value in two linkedlists
-    // if the values are not equal then retore the original ll and then retur false
-    // else finally restore the original linkedlist and return true
     public ListNode getMid(ListNode head) {
         ListNode slow=head;
         ListNode fast=head;
@@ -24,35 +19,29 @@ class Solution {
         return slow;
     }
     public ListNode reverse(ListNode head) {
+        ListNode temp=head;
         ListNode prev=null;
-        ListNode curr=head;
-        while(curr!=null) {
-            ListNode nextNode=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=nextNode;
+        while(temp!=null) {
+            ListNode nextNode=temp.next;
+            temp.next=prev;
+            prev=temp;
+            temp=nextNode;
         }
         return prev;
     }
     public boolean isPalindrome(ListNode head) {
         ListNode mid=getMid(head);
         ListNode nextNode=mid.next;
-        mid.next=null;
-        ListNode firstHead=head;
         ListNode secondHead=reverse(nextNode);
-        ListNode temp1=firstHead;
+        ListNode temp1=head;
         ListNode temp2=secondHead;
         while(temp2!=null) {
             if(temp1.val!=temp2.val) {
-                nextNode=reverse(secondHead);
-                mid.next=nextNode;
                 return false;
             }
             temp1=temp1.next;
             temp2=temp2.next;
         }
-        nextNode=reverse(secondHead);
-        mid.next=nextNode;
         return true;
     }
 }
