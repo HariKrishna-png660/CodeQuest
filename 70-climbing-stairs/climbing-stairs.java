f7939f16b119685c8956c04d8e3f9803fb42aedf
HariKrishna-png660 <htalagapu@gmail.com>
2026-03-19 18:13:56 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (96.18%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -57,17 +57,15 @@ class Solution {
     // =======================================
     // time complexity is O(n)
     // space complexity is O(1)
-    public int climbStairs(int n,int dp[]) {
-        if(n<=1) {
-            return 1;
-        } 
-        if(dp[n] != 0) {
-            return dp[n];
-        }
-        return dp[n]=climbStairs(n-1,dp)+climbStairs(n-2,dp);
-    }
     public int climbStairs(int n) {
         int dp[]=new int[n+1];
-        return climbStairs(n,dp);
+        for(int i=0;i<=n;i++) {
+            if(i <= 1) {
+                dp[i]=1;
+                continue;
+            }
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     } 
 }
