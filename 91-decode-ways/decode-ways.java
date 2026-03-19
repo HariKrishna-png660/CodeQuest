c49b269a1366c9f112a6f80a4622b9c5e8bbce02
HariKrishna-png660 <htalagapu@gmail.com>
2026-03-19 22:02:51 +0530

    Time: 1 ms (86.72%) | Memory: 43.4 MB (35.54%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,25 +1,36 @@
 class Solution {
-    public int numDecodings(String s) {
-        int n=s.length();
-        int firstNext=1;
-        int secondNext=0;
-        for(int i=n-1;i>=0;i--) {
-            if(s.charAt(i)=='0') {
-                secondNext=firstNext;
-                firstNext=0;
-                continue;
+    public boolean isValid(String curr) {
+         if(curr.charAt(0) != '0' && Integer.parseInt(curr) <= 26) {
+                 return true;
+         }
+         return false;
+    }
+    public int numDecodings(int idx,String s,String curr,int dp[]) {
+        if(idx==0) {
+            if(isValid(curr)) {
+                 return 1;
             }
-            int current=firstNext;
-            if(i+2<=n) {
-                  String sub=s.substring(i,i+2);
-                  int val=Integer.parseInt(sub);
-                   if(val<=26) {
-                      current += secondNext;
-                    }
-            }
-            secondNext=firstNext;
-            firstNext=current;
+            return 0;
         }
-        return firstNext;
+        if(curr.length() > 0 && !isValid(curr)) {
+            return 0;
+        }
+        if(dp[idx] != -1) {
+            return dp[idx];
+        }
+        int ways=0;
+        if(idx-1 >= 0) {
+            ways += numDecodings(idx-1,s,s.substring(idx-1,idx),dp);
+        }
+        if(idx-2 >= 0) {
+            ways += numDecodings(idx-2,s,s.substring(idx-2,idx),dp);
+        }
+        return dp[idx]=ways;
+    }
+    public int numDecodings(String s) {
+       int n=s.length();
+       int dp[]=new int[n+1];
+       Arrays.fill(dp,-1);
+       return numDecodings(n,s,"",dp);
     }
 }
