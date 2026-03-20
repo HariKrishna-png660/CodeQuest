646903f618ec3bbf145de2e82db2b96d78564f27
HariKrishna-png660 <htalagapu@gmail.com>
2026-03-20 17:03:38 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (94.75%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,7 +1,7 @@
 class Solution {
     // code written for leetcode 33 
     // will fail for test case 
-    // [1,0,1,1,1]
+    // [1,0,1,1,1] and [1, 1, 1, 0, 1] test case 
     // public boolean search(int[] nums, int target) {
     //     int n=nums.length;
     //     int si=0;
@@ -34,36 +34,36 @@ class Solution {
     //     return false;
     // }
      public boolean search(int[] nums, int target) {
-        int n=nums.length;
-        int si=0;
-        int ei=n-1;
-        while(si<=ei) {
+         int n=nums.length;
+         int si=0;
+         int ei=n-1;
+         while(si <= ei) {
              int mid=(si+ei)/2;
              if(nums[mid]==target || nums[si]==target) {
-                return true;
+                 return true;
              }
-             else if(nums[si]<nums[mid]) { // left sise half is sorted  and target may or may not exist here
-                 // checking for the target
-                 if(nums[si]<=target && nums[mid]>target) {
-                    ei=mid-1;
-                 }
-                 else {
-                    si=mid+1;
-                 }
+             else if(nums[si] < nums[mid]) { // left part is sorted
+                   // search wheather target exists here or not
+                   if(nums[si] <= target && nums[mid] > target) {
+                       ei=mid-1;
+                   }
+                   else {
+                       si=mid+1;
+                   }
              }
-             else if(nums[mid]<nums[ei]) {  
-                // check whether 
-                if(nums[mid]<target && nums[ei]>=target) {
-                    si=mid+1;
-                }
-                else {
-                    ei=mid-1;
-                }
+             else if(nums[mid] < nums[ei]) { // right part is sorted
+                  // search wheather target exists here or not
+                  if(nums[mid] < target && nums[ei] >= target) {
+                     si=mid+1;
+                  }
+                  else {
+                     ei=mid-1;
+                  }
              }
              else {
                 si++;
              }
-        }
-        return false;
+         }
+         return false;
      }
 }
