adb99adb56108589ce1f9a11d1b2edef1c8c9a2d
HariKrishna-png660 <htalagapu@gmail.com>
2026-03-20 16:22:44 +0530

    Time: 1 ms (25.29%) | Memory: 44.6 MB (69.71%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.java
@@ -1,28 +1,14 @@
 class Solution {
-    public int findInsertionIndex(int arr[],int k) {
+    public int findKthPositive(int[] arr, int k) {
         int n=arr.length;
-        int si=0;
-        int ei=n-1;
-        int ans=-1;
-        while(si <= ei) {
-            int mid=(si+ei)/2;
-            if(arr[mid]<k) {
-                ans=mid;
-                si=mid+1;
-            }
-            else {
-                ei=mid-1;
+        int index=-1;
+        for(int i=0;i<n;i++) {
+            int expectedNum=i+1;
+            int diff=arr[i]-expectedNum;
+            if(diff < k) {
+                index=i;
             }
         }
-        return ans;
-    }
-    public int findKthPositive(int[] arr, int k) {
-         int n=arr.length;
-         int diff[] =new int[n];
-         for(int i=1;i<=n;i++) {
-            diff[i-1]=arr[i-1]-i;
-         }
-         int index=findInsertionIndex(diff,k);
-         return index+k+1;
+        return index+1+k;
     }
 }
