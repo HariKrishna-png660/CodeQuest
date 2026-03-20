6b2fafe2c9ab7085a246e0c892922a4143609d89
HariKrishna-png660 <htalagapu@gmail.com>
2026-03-20 14:29:29 +0530

    Time: 5 ms (57.02%) | Memory: 47.8 MB (61.07%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,53 +1,49 @@
 class Solution {
-    public int findInsertPosition(int arr[],int tar) {
+    public int findInsertionPosition(int arr[],int tar) {
         int n=arr.length;
-        int low=0;
-        int high=n-1;
-        int pos=n;
-        while(low<=high) {
-            int mid=(low+high)/2;
+        int si=0;
+        int ei=n-1;
+        while(si <= ei) {
+            int mid=(si+ei)/2;
             if(arr[mid]==tar) {
                 return mid;
             }
-            else if(arr[mid]<tar) {
-                low=mid+1;
+            else if(arr[mid] < tar) {
+                si=mid+1;
             }
             else {
-                pos=mid;
-                high=mid-1;
+                ei=mid-1;
             }
         }
-        return pos;
+        return si;
     }
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-         int n=arr.length;
-         List<Integer> list=new ArrayList<>();
-         for(int i=0;i<n;i++) {
-            list.add(arr[i]);
-         }
-         // if the target is less than all the elements in the array
-         if(x<arr[0]) {
-            return list.subList(0,k);
-         }
-         // if the target is greater than all the elements in the array
-         if(x>arr[n-1]) {
-            return list.subList(n-k,n);
-         } 
-         else {
-            int pos=findInsertPosition(arr,x);
-            int si=Math.max(0,pos-k);
-            int ei=Math.min(n-1,pos+k);
-            while((ei-si+1)>k) {
-                int elementAtSi=list.get(si);
-                int elementAtEi=list.get(ei);
-                if(x-elementAtSi <= elementAtEi-x) {
-                    ei--;
-                }
-                else {
+          int n=arr.length;
+          List<Integer> ans=new ArrayList<>();
+          for(int i=0;i<n;i++) {
+              ans.add(arr[i]);
+          }
+          if(x<arr[0]) {
+             return ans.subList(0,k);
+          }
+          else if(x>arr[n-1]) {
+            return ans.subList(n-k,n);
+          }
+          else {
+             int pos=findInsertionPosition(arr,x);
+             int si=Math.max(0,pos-k);
+             int ei=Math.min(n-1,pos+k);
+             while((ei-si+1) > k) {
+                  int elementAtSi=arr[si];
+                  int elementAtEi=arr[ei];
+                  if(x-elementAtSi <= elementAtEi-x) {
+                     ei--;
+                  }
+                  else {
                     si++;
-                }
-            }
-            return list.subList(si,ei+1);
-         }
+                  }
+             }
+             return ans.subList(si,ei+1);
+          }
     }
 }
