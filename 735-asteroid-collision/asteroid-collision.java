8e8e695611be3e9e9e76da41485df203de8df447
HariKrishna-png660 <htalagapu@gmail.com>
2026-03-22 14:53:22 +0530

    Time: 5 ms (66.32%) | Memory: 47.2 MB (25.58%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,45 +1,44 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        // first intution is checking wheather the current element is positive or negative 
-        // if the current element is positive then it means that no asteriod before the current asteriod will collide with the current asteriod
-        // if the current asteriods is negative then we have two cases either the before asteriods is positive or negative
-        // if the before asteriod is +ve then it means that they are going to collide 
-        // the value of the before asteriod is greater than the current asteriod then current asteriod is going to die 
-        // the value of the before asteriod is less than the current asteriod then current asteriod is going to kill that asteriod and also check for before asteriods
-        // the value value of the asteriod is equal to the current asteriod then it means that the current asteriod and the before asteriod both are going to die 
-        // if the before asteriod is -ve then it means that they are not going to collide 
         int n=asteroids.length;
         Stack<Integer> st=new Stack<>();
-        // iterate on each of the element of the array
         for(int i=0;i<n;i++) {
-            int currentValue=asteroids[i];
-            if(currentValue>0) {
-                st.push(currentValue);
+            int currentVal=asteroids[i];
+            if(currentVal > 0) {
+                st.push(currentVal);
             }
             else {
-                boolean canPush=true;
-                while(st.size()>0 && st.peek()>0) {
-                    if(st.peek() < -(currentValue)) {
-                        st.pop();
-                    }
-                    else if(st.peek() == -(currentValue)) {
+               boolean canPush=true;
+               while(st.size() > 0 && st.peek()>0) {
+                   // current is a -ve and the number below it ca be +ve or neg
+                   // if it is neg then go out of the loop 
+                   // else we need to check for some magnitude values
+                   // if they are of same magni's then curr is not pushed into the stack
+                   // if below is of less magni then we pop 
+                   // if below is of more magni then we do nithing
+                   if(st.peek() == -(currentVal)) {
                         canPush=false;
                         st.pop();
                         break;
-                    }
-                    else {
-                        canPush=false;
-                        break;
-                    }
-                }
-                if(canPush) {
-                    st.push(currentValue);
-                }
+                   }
+                   else if(st.peek() < -(currentVal)) {
+                       canPush=true;
+                       st.pop();
+                   }
+                   else {
+                       canPush=false;
+                       break;
+                   }
+               }
+               if(canPush) {
+                  st.push(currentVal);
+               }
             }
         }
         int ans[]=new int[st.size()];
-        for(int i=st.size()-1;i>=0;i--) {
-            ans[i]=st.get(i);
+        int j=st.size()-1;
+        for(;j>=0;j--) {
+            ans[j]=st.pop();
         }
         return ans;
     }
